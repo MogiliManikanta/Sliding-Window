2c88c83055219f063fb9a42bed3824fd8fd62409
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-21 17:29:03 +0530

    Time: 2 ms (99.91%) | Memory: 43.4 MB (98.01%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,29 +2,29 @@ class Solution {
     public String minWindow(String s, String t) {
         int[] freq = new int[128];
         for(char c : t.toCharArray()){
-            freq[c]++;
+            freq[c]++;//storing the frequency
         }
         int start=0,end=0,minLength=Integer.MAX_VALUE,counter=t.length(),minStart=0;
         while(end<s.length()){
             char endChar = s.charAt(end);
             if(freq[endChar]>0){
-                counter--;
+                counter--;//if word matches in the both the string decrement the counter variable
             }
-            freq[endChar]--;
-            end++;
-            while(counter==0){
+            freq[endChar]--;//decrementing the counter variable
+            end++;//incrementhing the end variable
+            while(counter==0){//if all the matches then counter variable set to 0
                 if(minLength>end-start){
-                    minLength=end-start;
-                    minStart=start;
+                    minLength=end-start;//taking the minlength
+                    minStart=start;//minimum start
                 }
-                char startChar = s.charAt(start);
+                char startChar = s.charAt(start);//again iterate the from the start character 
                 freq[startChar]++;
-                if(freq[startChar]>0){
+                if(freq[startChar]>0){// if the frequency is greater than zero increment the counter 0
                     counter++;
                 }
-                start++;
+                start++;// increment the start operator
             }
         }
-        return minLength==Integer.MAX_VALUE?"":s.substring(minStart,minLength+minStart);
+        return minLength==Integer.MAX_VALUE?"":s.substring(minStart,minLength+minStart);// if no string not found return the empty and the return the substring
     }
 }
