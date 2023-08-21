ed5b9fb61f23c62bc7e86fa5173c116a4c697a73
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-21 05:33:22 +0530

    Time: 191 ms (49.34%) | Memory: 60.6 MB (70.81%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -1,24 +1,34 @@
+/* Approach : Sliding Window
+1. Store all forbidden words in unordered set.
+2. Use sliding window to find max length of valid substring
+Time : O(m * maxLen + n * (maxLen^2))       // set + Sliding Window         
+Space : O(m * maxLen + n * maxLen )         // set + temp string
+where, n = word.size(), m = forbidden.size(), maxLen = Max length of any forbidden word
+*/
 class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
         HashSet<String>set = new HashSet<>();
         int maxLenForbidden = Integer.MIN_VALUE;
+        // store forbidden words in set
         for(String temp : forbidden){
             set.add(temp);
             maxLenForbidden = Math.max(maxLenForbidden,temp.length());
         }
         int left=0,right=0;
         int longest=Integer.MIN_VALUE;
+        // sliding window
         while(right<word.length()){
+            // check for forbidden substrings after including l in current window
             String temp="";
-            for(int i=right;i>=left && i>(right-maxLenForbidden);i--){
+            for(int i=right;i>=left && i>(right-maxLenForbidden);i--){// need to check only upto maxLen
                 temp=word.charAt(i)+temp;
-                if(set.contains(temp)){
-                    left=i+1;
+                if(set.contains(temp)){// temp present in forbidden words
+                    left=i+1;// ith char can't be present in valid window, update 
                     break;
                 }
             }
-            right++;
-            longest=Math.max(longest,right-left);
+            right++;// slide right boundary for next iteration
+            longest=Math.max(longest,right-left);// update ans for current window
         }
         return longest;
     }
