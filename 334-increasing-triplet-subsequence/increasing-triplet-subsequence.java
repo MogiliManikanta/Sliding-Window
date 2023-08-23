85714fce0b5de9174d922c11bb870f50b3c469a0
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-23 07:38:18 +0530

    Time: 2 ms (94.04%) | Memory: 131.9 MB (20.92%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
+        // start with two largest values, as soon as we find a number bigger than both, while both have been updated, return true
         int firstNum = Integer.MAX_VALUE;
         int secondNum = Integer.MAX_VALUE;
         for(int n : nums){
-            if(n<=firstNum){
+            if(n<=firstNum){ // update small if n is smaller than both
                 firstNum=n;
             }
-            else if(n<=secondNum){
+            else if(n<=secondNum){ // update big only if greater than small but smaller than big
                 secondNum=n;
             }
             else{
-                return true;
+                return true; // return if you find a number bigger than both
             }
         }
         return false;
