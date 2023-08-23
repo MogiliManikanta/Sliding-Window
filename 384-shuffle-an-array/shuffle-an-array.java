2f964168ac8339792d9d1332edd443fefd1a522b
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-23 07:21:52 +0530

    Time: 55 ms (60.67%) | Memory: 49.6 MB (59.08%) - LeetSync

--- 384-shuffle-an-array/shuffle-an-array.java
+++ 384-shuffle-an-array/shuffle-an-array.java
@@ -1,31 +1,27 @@
 class Solution {
-	int[] nums;
-	public Solution(int[] nums) {
-		this.nums = nums;
-	}
-	
-	/** Resets the array to its original configuration and return it. */
-	public int[] reset() {
-		return nums;
-	}
-	
-	/** Returns a random shuffling of the array. */
-	public int[] shuffle() {
-		int[] res = new int[nums.length];
-		res = nums.clone();
-		
-		for(int i = 0; i < nums.length; i++){
-			int rand = (int)(Math.random()*(i+1));
-			swap(res, i, rand);
-		}
-		return res;
-	}
-	
-	public void swap(int[] nums, int i, int j){
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
-	}
+    int[] nums;
+    public Solution(int[] nums) {
+        this.nums=nums;
+    }
+    
+    public int[] reset() {
+        return nums;
+    }
+    
+    public int[] shuffle() {
+        int result[] = new int[nums.length];
+        result = nums.clone();
+        for(int i=0;i<nums.length;i++){
+            int rand = (int)(Math.random()*(i+1));
+            swap(result,i,rand);
+        }
+        return result;
+    }
+    public void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
 
 /**
