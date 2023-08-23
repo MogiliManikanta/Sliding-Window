240a34e568144031290bbb1c0adbce8d8fe4ba12
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-23 19:44:31 +0530

    Time: 5 ms (92.33%) | Memory: 49.3 MB (41.71%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null || matrix.length<1 || matrix[0].length<1){
+            return false;
+        }
+        int row=0;
+        int col=matrix[0].length-1;
+        while(col>=0 && row<matrix.length){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
