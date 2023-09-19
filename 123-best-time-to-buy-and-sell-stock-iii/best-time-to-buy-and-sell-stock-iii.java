27bef8d5f7674d791062df02309b0f01488fb794
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-20 03:22:59 +0530

    Time: 3 ms (90.93%) | Memory: 55.8 MB (82.49%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int maxProfit(int[] prices) {
+    int t1Cost = Integer.MAX_VALUE, 
+        t2Cost = Integer.MAX_VALUE;
+    int t1Profit = 0,
+        t2Profit = 0;
+
+    for (int price : prices) {
+        // the maximum profit if only one transaction is allowed
+        t1Cost = Math.min(t1Cost, price);
+        t1Profit = Math.max(t1Profit, price - t1Cost);
+        // reinvest the gained profit in the second transaction
+        t2Cost = Math.min(t2Cost, price - t1Profit);
+        t2Profit = Math.max(t2Profit, price - t2Cost);
+    }
+
+    return t2Profit;
+  }
+}
