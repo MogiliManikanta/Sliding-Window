76176d7a21f1ed673f6cc2230c9c1b7ae9571761
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-20 03:46:01 +0530

    Time: 5 ms (85.88%) | Memory: 43.3 MB (83.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,16 +1,35 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length()==0) return 0;
+        // Check if the input string is empty, return 0 if it is.
+        if (s.length() == 0)
+            return 0;
+        
+        // Create a HashMap to store characters and their positions.
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max=0;
-        for (int i=0, j=0; i<s.length(); ++i){
-            if (map.containsKey(s.charAt(i))){
-                j = Math.max(j,map.get(s.charAt(i))+1);
-                //j=map.get(s.charAt(i))+1;
+        
+        // Initialize variables to keep track of the maximum length and the start of the substring.
+        int max = 0;  // This will store the length of the longest substring.
+        int j = 0;    // This is the start of the current substring.
+        
+        // Loop through the characters of the input string.
+        for (int i = 0; i < s.length(); ++i) {
+            // Check if the character at position 'i' is already in the HashMap.
+            if (map.containsKey(s.charAt(i))) {
+                // If it is, update 'j' to the maximum of its current value and (previous position + 1).
+                // This ensures that 'j' moves to the position right after the repeated character.
+                 j = Math.max(j, map.get(s.charAt(i)) + 1);
+                // // Alternatively, you can use the line below instead of the Math.max line:
+                //  j = map.get(s.charAt(i)) + 1;
             }
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+            
+            // Put the current character and its position into the HashMap.
+            map.put(s.charAt(i), i);
+            
+            // Update 'max' with the maximum length of the current substring.
+            max = Math.max(max, i - j + 1);
         }
+        
+        // Return the length of the longest substring without repeating characters.
         return max;
     }
 }
