241f796afdc6a30d6f01d303eafd9314d0e82bda
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-21 03:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (63.30%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,29 @@
+class Solution {
+   /* the order is: prev2, prev1, num  */
+public int rob(int[] nums) {
+    if (nums.length == 0) return 0;
+    int prev1 = 0;
+    int prev2 = 0;
+    for (int num : nums) {
+        int tmp = prev1;
+        prev1 = Math.max(prev2 + num, prev1);
+        prev2 = tmp;
+    }
+    return prev1;
+}
+}
+/**
+This particular problem and most of others can be approached using the following sequence:
+
+Find recursive relation
+Recursive (top-down)
+Recursive + memo (top-down)
+Iterative + memo (bottom-up)
+Iterative + N variables (bottom-up)
+
+robbery of current house + loot from houses before the previous
+loot from the previous house robbery and any loot captured before that
+rob(i) = Math.max( rob(i - 2) + currentHouseValue, rob(i - 1) )
+
+*/
+
