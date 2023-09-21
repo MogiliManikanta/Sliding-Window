e7818d788c1a121ba048592b28bf3b0b17fa11ae
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-21 18:46:46 +0530

    Time: 2 ms (84.25%) | Memory: 45.7 MB (74.19%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -8,40 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution 
-{
-    public ListNode reverse(ListNode head)
-    {
-        ListNode prev=null,curr=head,next=null;
-        while(curr!=null)
-        {
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+class Solution {
+    // Helper function to reverse a linked list.
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;  // Store the next node.
+            curr.next = prev;  // Reverse the current node's next pointer.
+            prev = curr;       // Move prev to the current node.
+            curr = next;       // Move curr to the next node.
         }
-        return prev;
+        return prev;  // Return the new head of the reversed list.
     }
-    public void reorderList(ListNode head) 
-    {
-        ListNode slow=head;
-        ListNode fast=head.next;
-        while(fast!=null && fast.next!=null)
-        {
-            slow=slow.next;
-            fast=fast.next.next;
+    
+    public void reorderList(ListNode head) {
+        // Initialize two pointers, slow and fast.
+        ListNode slow = head;
+        ListNode fast = head; //(head != null) ? head.next : null;
+        
+        // Move slow one step at a time and fast two steps at a time until fast reaches the end.
+        while (fast != null && fast.next != null) {
+            slow = slow.next;     // Move slow one step.
+            fast = fast.next.next; // Move fast two steps.
         }
-        ListNode second=reverse(slow.next);
-        slow.next=null;
-        ListNode first=head;
-        while(second!=null)
-        {
-            ListNode temp1=first.next;
-            ListNode temp2=second.next;
-            first.next=second;
-            second.next=temp1;
-            first=temp1;
-            second=temp2;
+        
+        // After the loop, slow is at the middle of the linked list.
+        // Reverse the second half of the linked list.
+        ListNode second = reverse(slow.next);
+        slow.next = null; // Cut off the first half from the second half.
+        
+        ListNode first = head;
+        while (second != null) {
+            ListNode temp1 = first.next; // Store the next node in the first half.
+            ListNode temp2 = second.next; // Store the next node in the second half.
+            
+            first.next = second; // Connect the current node in the first half to the second half.
+            second.next = temp1; // Connect the current node in the second half to the first half.
+            
+            first = temp1; // Move the first pointer to the next node in the first half.
+            second = temp2; // Move the second pointer to the next node in the second half.
         }
     }
 }
