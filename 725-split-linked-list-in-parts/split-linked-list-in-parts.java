2cbd1db4fa240559ecfe7b616f73565877ff21ac
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-22 19:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (55.75%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.java
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -9,21 +9,43 @@
  * }
  */
 class Solution {
-    public ListNode[] splitListToParts(ListNode root, int k) {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        // Create an array to store the resulting list parts
         ListNode[] parts = new ListNode[k];
-        int len = 0;
-        for (ListNode node = root; node != null; node = node.next)
-            len++;
-        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
-        ListNode node = root, prev = null;
-        for (int i = 0; node != null && i < k; i++, r--) {
-            parts[i] = node;
-            for (int j = 0; j < n + (r > 0 ? 1 : 0); j++) {
-                prev = node;
-                node = node.next;
+        
+        // Calculate the length of the original linked list
+        int length = 0;
+        for (ListNode currentNode = head; currentNode != null; currentNode = currentNode.next)
+            length++;
+        
+        // Calculate the minimum guaranteed size of each part and the number of extra nodes
+        int minSize = length / k;
+        int extraNodes = length % k;
+        
+        // Initialize pointers for traversing the original list and a previous node
+        ListNode currentNode = head;
+        ListNode previousNode = null;
+        
+        // Loop through each part
+        for (int i = 0; currentNode != null && i < k; i++, extraNodes--) {
+            // Store the current node as a part of the result
+            parts[i] = currentNode;
+            
+            // Determine the number of nodes to include in this part (either minSize or minSize + 1)
+            int nodesInPart = minSize + (extraNodes > 0 ? 1 : 0);
+            
+            // Traverse the list and update pointers accordingly
+            for (int j = 0; j < nodesInPart; j++) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
             }
-            prev.next = null;
+            
+            // Set the next pointer of the previous node to null to separate the part from the rest of the list
+            if (previousNode != null)
+                previousNode.next = null;
         }
+        
+        // Return the array of list parts
         return parts;        
     }
 }
