e79e1eba38990df76b301b0936303408090a52f9
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-23 12:46:02 +0530

    Time: 4 ms (100.00%) | Memory: 97 MB (5.34%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -10,15 +10,20 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        for(ListNode p_z = head,p=head.next;p!=null;p=p.next){
-            if(p.val!=0){
-                p_z.val+=p.val;
-            }
-            else{
-                p_z.next = p.next!=null?p:null;
-                p_z=p;
+        // Initialize two pointers: prevNonZero (for tracking nodes with non-zero values) and current (for iterating through the list)
+        for (ListNode prevNonZero = head, current = head.next; current != null; current = current.next) {
+            // Check if the value of the current node is not equal to 0
+            if (current.val != 0) {
+                // If it's not equal to 0, add the value to the previous non-zero value node's value
+                prevNonZero.val += current.val;
+            } else {
+                // If the value is 0, skip the current node by updating the next pointer of the previous non-zero value node
+                prevNonZero.next = (current.next != null) ? current : null;
+                // Move the prevNonZero pointer to the current node if its value is 0
+                prevNonZero = current;
             }
         }
+        // Return the modified linked list
         return head;
     }
 }
