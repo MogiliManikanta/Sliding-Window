21a1a0c8aaa5baf63dd451b79266a385f9b55f5d
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-24 16:30:20 +0530

    Time: 3 ms (91.54%) | Memory: 43.5 MB (88.15%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,30 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
+        if(t.length()>s.length()){
+            return "";
+        }
+
         int[] freq = new int[128];
         for(char c : t.toCharArray()){
-            freq[c]++;//storing the frequency
+            freq[c]++;
         }
-        int start=0,end=0,minLength=Integer.MAX_VALUE,counter=t.length(),minStart=0;
+        int start=0,end=0,minLen=Integer.MAX_VALUE,minStart=0;
+        int counter=t.length();
         while(end<s.length()){
             char endChar = s.charAt(end);
+            
             if(freq[endChar]>0){
-                counter--;//if word matches in the both the string decrement the counter variable
+                counter--;
             }
-            freq[endChar]--;//decrementing the counter variable
-            end++;//incrementhing the end variable
-            while(counter==0){//if all the matches then counter variable set to 0
-                if(minLength>end-start){
-                    minLength=end-start;//taking the minlength
-                    minStart=start;//minimum start
+            freq[endChar]--;
+            end++;
+            while(counter==0){
+                if(minLen>end-start){
+                    minLen=end-start;
+                    minStart=start;
                 }
-                char startChar = s.charAt(start);//again iterate the from the start character 
+                char startChar = s.charAt(start);
                 freq[startChar]++;
-                if(freq[startChar]>0){// if the frequency is greater than zero increment the counter 0
+                if(freq[startChar]>0){
                     counter++;
                 }
-                start++;// increment the start operator
+                start++;
             }
         }
-        return minLength==Integer.MAX_VALUE?"":s.substring(minStart,minLength+minStart);// if no string not found return the empty and the return the substring
+        return minLen==Integer.MAX_VALUE?"":s.substring(minStart,minLen+minStart);
     }
 }
