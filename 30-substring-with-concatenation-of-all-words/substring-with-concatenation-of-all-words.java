eedb67a42b97bfe1a2ab12ee0ee9404d1a371eb4
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-24 11:55:41 +0530

    Time: 1514 ms (51.56%) | Memory: 45.2 MB (22.54%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,28 +1,37 @@
 class Solution {
-    public static List<Integer> findSubstring(String S, String[] L) {
-    List<Integer> res = new ArrayList<Integer>();
-    if (S == null || L == null || L.length == 0) return res;
-    int len = L[0].length(); // length of each word
-    
-    Map<String, Integer> map = new HashMap<String, Integer>(); // map for L
-    for (String w : L) map.put(w, map.containsKey(w) ? map.get(w) + 1 : 1);
-    
-    for (int i = 0; i <= S.length() - len * L.length; i++) {
-        Map<String, Integer> copy = new HashMap<String, Integer>(map);
-        for (int j = 0; j < L.length; j++) { // checkc if match
-            String str = S.substring(i + j*len, i + j*len + len); // next word
-            if (copy.containsKey(str)) { // is in remaining words
-                int count = copy.get(str);
-                if (count == 1) copy.remove(str);
-                else copy.put(str, count - 1);
-                if (copy.isEmpty()) { // matches
-                    res.add(i);
+    public List<Integer> findSubstring(String s, String[] words) {
+        if(s.length()==0 || s==null){
+            return new ArrayList<>();
+        }
+        ArrayList<Integer>result= new ArrayList<>();
+        int len = words.length;
+        int wordlen = words[0].length();
+        HashMap<String,Integer>map = new HashMap<>();
+        for(String l : words){
+            map.put(l,map.getOrDefault(l,0)+1);
+        }
+        for(int i=0;i<=s.length()-len*wordlen;i++){
+            HashMap<String,Integer>copy = new HashMap<>(map);
+            for(int j=0;j<len;j++){
+                String str = s.substring(i+j*wordlen,i+j*wordlen+wordlen);
+                if(copy.containsKey(str)){
+                    int count = copy.get(str);
+                    if(count==1){
+                        copy.remove(str);
+                    }
+                    else{
+                        copy.put(str,count-1);
+                    }
+                    if(copy.isEmpty()){
+                        result.add(i);
+                        break;
+                    }
+                }
+                else{
                     break;
                 }
             }
-             else break; // not in L
         }
+        return result;
     }
-    return res;
-}
 }
