6f8d13ade90624107f1c483d545190e3edbe6186
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-24 11:56:06 +0530

    Time: 1624 ms (46.58%) | Memory: 44.7 MB (76.95%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,37 +1,51 @@
-class Solution {
-    public List<Integer> findSubstring(String s, String[] words) {
-        if(s.length()==0 || s==null){
-            return new ArrayList<>();
-        }
-        ArrayList<Integer>result= new ArrayList<>();
-        int len = words.length;
-        int wordlen = words[0].length();
-        HashMap<String,Integer>map = new HashMap<>();
-        for(String l : words){
-            map.put(l,map.getOrDefault(l,0)+1);
-        }
-        for(int i=0;i<=s.length()-len*wordlen;i++){
-            HashMap<String,Integer>copy = new HashMap<>(map);
-            for(int j=0;j<len;j++){
-                String str = s.substring(i+j*wordlen,i+j*wordlen+wordlen);
-                if(copy.containsKey(str)){
-                    int count = copy.get(str);
-                    if(count==1){
-                        copy.remove(str);
-                    }
-                    else{
-                        copy.put(str,count-1);
-                    }
-                    if(copy.isEmpty()){
-                        result.add(i);
-                        break;
-                    }
+class Solution{
+public static List<Integer> findSubstring(String S, String[] L) {
+    List<Integer> res = new ArrayList<Integer>(); // Create a list to store the results.
+    
+    // Check for edge cases.
+    if (S == null || L == null || L.length == 0) return res;
+
+    int len = L[0].length(); // Get the length of each word in L.
+
+    // Create a map to store the frequency of each word in L.
+    Map<String, Integer> map = new HashMap<String, Integer>();
+    for (String w : L) {
+        // If the word is already in the map, increment its count; otherwise, add it with a count of 1.
+        map.put(w, map.containsKey(w) ? map.get(w) + 1 : 1);
+    }
+
+    // Iterate through S from index 0 to a point where there are enough characters left for L.
+    for (int i = 0; i <= S.length() - len * L.length; i++) {
+        // Create a copy of the map for each iteration to avoid modifying the original map.
+        Map<String, Integer> copy = new HashMap<String, Integer>(map);
+
+        // Check if the current substring of S starting at index 'i' can form a valid concatenation.
+        for (int j = 0; j < L.length; j++) {
+            // Extract the next word from S.
+            String str = S.substring(i + j * len, i + j * len + len);
+
+            // Check if the word is in the copy of the map.
+            if (copy.containsKey(str)) {
+                int count = copy.get(str);
+
+                if (count == 1) {
+                    copy.remove(str); // If there's only one instance of the word, remove it from the copy.
+                } else {
+                    copy.put(str, count - 1); // Otherwise, decrement its count in the copy.
                 }
-                else{
+
+                if (copy.isEmpty()) {
+                    // If the copy becomes empty, it means all words in L have been matched, so add the index 'i' to the result.
+                    res.add(i);
                     break;
                 }
+            } else {
+                // If the current word is not in L, break out of the loop.
+                break;
             }
         }
-        return result;
     }
+
+    return res; // Return the list of starting indices where valid concatenations were found.
+}
 }
