c4fd0bff3bb26f60bbf61630c093aac5b1d53c64
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-09-26 20:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (56.30%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public int findMin(int[] nums) 
+    {
+        int low = 0;
+        int high = nums.length-1;
+        // loop invariant: 1. low < high
+        //                 2. mid != high and thus A[mid] != A[high] (no duplicate exists)
+        //                 3. minimum is between [low, high]
+        // The proof that the loop will exit: after each iteration either the 'high' decreases
+        // or the 'low' increases, so the interval [low, high] will always shrink.
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[high])
+                // the mininum is in the left part
+                high = mid;
+            else if (nums[mid] > nums[high])
+                // the mininum is in the right part
+                low = mid + 1;
+        }
+
+        return nums[low];
+    }
+}
