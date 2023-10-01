d20bfa45ca444116434c2e60036948d4597da773
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-10-02 04:07:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (17.22%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,36 +14,59 @@ class Node {
 */
 
 class Solution {
-
     public Node copyRandomList(Node head) {
+        // Check if the input linked list is empty. If so, return null.
         if (head == null) return head;
+
+        // Step 1: Duplicate each node and insert it between the original nodes.
         Node temp = head;
         while (temp != null) {
+            // Create a new node with the same value as the original node.
             Node tempNew = new Node(temp.val);
+            
+            // Connect the new node to the next node in the original list.
             tempNew.next = temp.next;
+            
+            // Update the original node's next pointer to point to the new node.
             temp.next = tempNew;
+            
+            // Move to the next node in the original list (skip the duplicated node).
             temp = temp.next.next;
         }
-        // Update the random pointer
+
+        // Step 2: Update the random pointers of the duplicated nodes.
+        // Set the newHead as the head of the duplicated list.
         Node newHead = head.next;
         temp = head;
         while (temp != null) {
+            // Get the duplicated node (the one just after the original node).
             Node next = temp.next;
+            
+            // If the original node has a random pointer, update the duplicated node's random pointer.
             if (temp.random != null) 
                 next.random = temp.random.next;
+            
+            // Move to the next original node (skip the duplicated node).
             temp = next.next;
         }
-        // Unwinding the list or separating the two lists
+
+        // Step 3: Separate the original list from the duplicated list.
         temp = head;
         while (temp != null) {
-            // temp.next = temp.next.next;
+            // Restore the original list's next pointer.
             Node copy = temp.next;
             temp.next = copy.next;
+            
+            // If there's a node after the duplicated node, update its next pointer.
             if (copy.next != null) {
                 copy.next = temp.next.next;
             }
+            
+            // Move to the next original node (skip the duplicated node).
             temp = temp.next;
         }
+
+        // Return the head of the duplicated list.
         return newHead;
     }
 }
