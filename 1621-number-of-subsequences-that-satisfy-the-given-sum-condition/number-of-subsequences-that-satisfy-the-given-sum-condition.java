8c275388e498cdcd9099fe6aed2e42c5106b8305
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-10-30 19:30:11 +0530

    Time: 28 ms (77.96%) | Memory: 55.2 MB (67.33%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,26 @@
+class Solution {
+      public int numSubseq(int[] A, int target) {
+        Arrays.sort(A);
+        int res = 0, n = A.length, left = 0, right = n - 1, mod = (int)1e9 + 7;
+        int[] pows = new int[n];
+        pows[0] = 1;
+        for (int i = 1 ; i < n ; ++i)
+            pows[i] = pows[i - 1] * 2 % mod;
+        while (left <= right) {
+            if (A[left] + A[right] > target) {
+                right--;
+            } else {
+                res = (res + pows[right - left]) % mod;
+                left++;
+            }
+        }
+        return res;
+    }
+}
+/***
+
+We don't care the original elements order,
+we only want to know the count of sub sequence.
+So we can sort the original A, and the result won't change.
+
+ */
