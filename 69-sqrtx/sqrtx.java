7f6ba4b1c123b8f550bd86a2e145e5dc21aa5bb5
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-02 19:28:07 +0530

    Time: 1 ms (97.13%) | Memory: 39.2 MB (87.20%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -18,3 +18,14 @@ class Solution {
     return left - 1;
     }
 }
+/**  
+  
+  
+  There's one thing I'd like to point out. Remember I say that we usually look for the minimal k value satisfying certain condition? But in this problem we are searching for maximal k value instead. Feeling confused? Don't be. Actually, the maximal k satisfying condition(k) is False is just equal to the minimal k satisfying condition(k) is True minus one. This is why I mentioned earlier that we need to decide which value to return, left or left - 1
+  
+  
+  
+  
+  
+  
+   */
