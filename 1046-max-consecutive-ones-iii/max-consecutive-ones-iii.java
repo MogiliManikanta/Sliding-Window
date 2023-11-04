0c89b617f26613529f7cde5084de1c0a720201c3
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-04 15:16:39 +0530

    Time: 5 ms (12.59%) | Memory: 46.6 MB (18.51%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,26 +1,15 @@
 class Solution {
     public int longestOnes(int[] A, int K) {
-    int left = 0;
-    int right = 0;
-    int max = 0;
-    
-    int numZeroes = 0;
-    for (right= 0; right<A.length; right++) {
-
-        if (A[right]==0) numZeroes++;
-        
-        if (numZeroes > K) {
-            if (A[left]==0) numZeroes--;
-            left ++;
+        int[] freq = new int[2];
+        int i = 0, r=0;
+        for(int j = 0; j < A.length; j++){
+            freq[A[j]] ++;
+            while(freq[0] > K && i <= j){
+                freq[A[i]] --;
+                i++;
+            }
+            r = Math.max(r, j-i+1);
         }
-        if (numZeroes <= K) {
-            // this is probably what I could come up during interview...
-            max = Math.max(max, right-left +1 );
-        }
+        return r;
     }
-    return max;
 }
-}
-/**For each A[j], try to find the longest subarray.
-If A[i] ~ A[j] has zeros <= K, we continue to increment j.
-If A[i] ~ A[j] has zeros > K, we increment i (as well as j). */
