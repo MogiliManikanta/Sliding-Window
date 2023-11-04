7384d2802cb5412c4da9104d8601cf6595e9ec46
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-05 05:20:07 +0530

    Time: 2 ms (99.92%) | Memory: 42.6 MB (93.20%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,43 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-      int n = nums.length;
-      int temp[] = new int[n];
-      int k=0;
-      temp[k]=nums[0];
-      for(int i=1;i<n;i++){
-          if(nums[i]<temp[k]){
-              int lowerbound=0,upperbound=k;
-              while(lowerbound <= upperbound){
-                  int mid = (lowerbound + upperbound)/2;
-                  if(temp[mid]<nums[i]){
-                      lowerbound=mid+1;
-                  }
-                  else{
-                      upperbound=mid-1;
-                  }
-              }
-              temp[lowerbound]=nums[i];
-          }
-          else if(nums[i]!=temp[k]){
-              k++;
-              temp[k]=nums[i];
-          }
+        // Get the length of the input array
+        int n = nums.length;
+        
+        // Create an array 'temp' to store the longest increasing subsequence
+        int temp[] = new int[n];
+        
+        // Initialize a variable 'k' to keep track of the current position in 'temp'
+        int k = 0;
+        
+        // Initialize the first element of 'temp' with the first element of 'nums'
+        temp[k] = nums[0];
+        
+        // Loop through the elements of 'nums' starting from the second element (index 1)
+        for (int i = 1; i < n; i++) {
+            // Check if the current element in 'nums' is smaller than the last element in 'temp'
+            if (nums[i] < temp[k]) {
+                // If it is smaller, perform a binary search to find the correct position
+                int lowerbound = 0;
+                int upperbound = k;
+                while (lowerbound <= upperbound) {
+                    int mid = (lowerbound + upperbound) / 2;
+                    if (temp[mid] < nums[i]) {
+                        lowerbound = mid + 1;
+                    } else {
+                        upperbound = mid - 1;
+                    }
+                }
+                // Update 'temp' with the current element at the correct position
+                temp[lowerbound] = nums[i];
+            } else if (nums[i] != temp[k]) {
+                // If the current element is not equal to the last element in 'temp' and not smaller,
+                // increment 'k' and add the current element to 'temp'
+                k++;
+                temp[k] = nums[i];
+            }
         }
-        return k+1;
-
+        
+        // The value of 'k' represents the length of the longest increasing subsequence
+        return k + 1;
     }
 }
