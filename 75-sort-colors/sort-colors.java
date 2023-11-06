8f9d30c3479dd1b8b73134f7b0f4c633ab95afac
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-07 04:29:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (54.56%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,33 +1,32 @@
 class Solution {
      public void sortColors(int[] nums) {
-    //     int n=nums.length;
-    //     int low =0;
-    //     int  mid=0;
-    //     int high=n-1;
-    //     while(mid<=high){
-    //         if(nums[mid]==0){
-    //             swap(nums,low,mid);
-    //             low++;
-    //             mid++;
-    //         }
-    //         else if(nums[mid]==1){
-    //             mid++;
-    //         }
-    //             /*
-    //                 0 0 0 0 0 0 0 0 1 1 1 2 2 2 2
-    //                                 l   h m
+        int n=nums.length;
+        int low =0;
+        int  mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+                /*
+                    0 0 0 0 0 0 0 0 1 1 1 2 2 2 2
+                                    l   h m
                 
-    //              */
-    //         else{
-    //             swap(nums,mid,high);
-    //             high--;
-    //         }
-    //     }
-    // }
-    // public  void swap(int[] nums,int  i,int j){
-    //     int temp =nums[i];
-    //     nums[i]=nums[j];
-    //     nums[j]=temp;
-        Arrays.sort(nums);
+                 */
+            else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+    public  void swap(int[] nums,int  i,int j){
+        int temp =nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
      }
 }
