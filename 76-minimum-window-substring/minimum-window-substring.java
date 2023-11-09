e183b30d32eda67c96381ca159befb3f120e8a53
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-09 17:52:14 +0530

    Time: 3 ms (90.60%) | Memory: 43.9 MB (64.98%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,30 +1,24 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        int[] freq = new int[128];
-        for(char c : t.toCharArray()){
-            freq[c]++;//storing the frequency
-        }
-        int start=0,end=0,minLength=Integer.MAX_VALUE,counter=t.length(),minStart=0;
-        while(end<s.length()){
-            char endChar = s.charAt(end);
-            if(freq[endChar]>0){
-                counter--;//if word matches in the both the string decrement the counter variable
+    public String minWindow(String input, String pattern) {
+    int[] frequencyMap = new int[128];
+    for (char c : pattern.toCharArray()) frequencyMap[c]++;
+    
+    int remainingChars = pattern.length(), start = 0, end = 0, minWindowSize = Integer.MAX_VALUE, minWindowStart = 0;
+    
+    while (end < input.length()) {
+        if (frequencyMap[input.charAt(end++)]-- > 0) remainingChars--;
+        
+        while (remainingChars == 0) {
+            if (end - start < minWindowSize) {
+                minWindowSize = end - start;
+                minWindowStart = start;
             }
-            freq[endChar]--;//decrementing the counter variable
-            end++;//incrementhing the end variable
-            while(counter==0){//if all the matches then counter variable set to 0
-                if(minLength>end-start){
-                    minLength=end-start;//taking the minlength
-                    minStart=start;//minimum start
-                }
-                char startChar = s.charAt(start);//again iterate the from the start character 
-                freq[startChar]++;
-                if(freq[startChar]>0){// if the frequency is greater than zero increment the counter 0
-                    counter++;
-                }
-                start++;// increment the start operator
-            }
+            
+            if (frequencyMap[input.charAt(start++)]++ == 0) remainingChars++;
         }
-        return minLength==Integer.MAX_VALUE?"":s.substring(minStart,minLength+minStart);// if no string not found return the empty and the return the substring
     }
+    
+    return minWindowSize == Integer.MAX_VALUE ? "" : input.substring(minWindowStart, minWindowStart + minWindowSize);
+}
+
 }
