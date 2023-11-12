08867235a5d9ddd5c8832d1ef4b0cb252accefb7
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-12 11:32:10 +0530

    Time: 267 ms (36.67%) | Memory: 43.8 MB (24.89%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 
     public int recursive(int[] nums,int target,int sum,int ind){
         if(ind==nums.length){
@@ -14,5 +14,32 @@ class Solution {
     }
     public int findTargetSumWays(int[] nums, int target) {
         return recursive(nums,target,0,0);
+    }
+}*/
+
+public class Solution {
+    public int recursive(int[] nums, int target, int sum, int ind, int[][] memo) {
+        if (ind == nums.length) {
+            if (target == sum) {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (memo[ind][sum + 1000] != 0) {
+            return memo[ind][sum + 1000];
+        }
+
+        int positive = recursive(nums, target, sum + nums[ind], ind + 1, memo);
+        int negative = recursive(nums, target, sum - nums[ind], ind + 1, memo);
+
+        memo[ind][sum + 1000] = negative + positive;
+
+        return memo[ind][sum + 1000];
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int[][] memo = new int[nums.length][2001]; // Range of sum is -1000 to 1000
+
+        return recursive(nums, target, 0, 0, memo);
     }
 }
