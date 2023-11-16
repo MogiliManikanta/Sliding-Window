5c344f2f1fc848557d3e1f7cc0f7ceea8c207328
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-16 09:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (31.27%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -14,9 +14,23 @@ class Solution {
         return dp[index] = Math.max(pick,notpick);
     }
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-       return helper(nums,nums.length-1,dp); 
+        /* First Approach
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp,-1);
+            return helper(nums,nums.length-1,dp); 
+       */
+       int n=nums.length;
+       int[] dp = new int[n];
+       dp[0]=nums[0];
+       for(int i=1;i<n;i++){
+           int pick=nums[i];
+           if(i>1){
+               pick+=dp[i-2];
+           }
+           int notpick = 0+dp[i-1];
+           dp[i]=Math.max(pick,notpick);
+       }
+       return dp[n-1];
     }
 }
 /**class Solution {
