e55357efa5f9b58d223b2e70046d5430124538dd
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-17 19:17:05 +0530

    Time: 3 ms (19.76%) | Memory: 41.5 MB (8.69%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -20,9 +20,27 @@ class Solution {
 
         return dp[si]= Math.min(one_day,Math.min(seven_day,thirty_day));
     }
-    public int mincostTickets(int[] days, int[] costs) {
-        int[] dp = new int[367];
-        Arrays.fill(dp,-1);
-         return sub(days,costs,0,dp);
+    public int mincostTickets(int[] days, int[] cost) {
+        // int[] dp = new int[367];
+        // Arrays.fill(dp,-1);
+        //  return sub(days,costs,0,dp);
+         HashSet<Integer> daySet = new HashSet<>();
+        for (int day : days) {
+            daySet.add(day);
+        }
+        int[] dp = new int[366];
+        for (int i = 1; i < 366; i++) {
+            dp[i] = dp[i - 1];
+            if (daySet.contains(i)) {
+                dp[i] = Math.min(
+                        dp[i - 1] + cost[0],// 1st case
+                        Math.min(
+                                dp[Math.max(0, i - 7)] + cost[1],   // 2nd case
+                                dp[Math.max(0, i - 30)] + cost[2]   // 3rd case
+                        )
+                );
+            }
+        }
+        return dp[365];
     }
 }
