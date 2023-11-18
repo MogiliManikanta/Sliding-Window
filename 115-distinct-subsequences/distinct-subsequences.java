34c3cf1dd56cda125448a25b6d8be0370377a8cb
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-18 18:00:23 +0530

    Time: 16 ms (44.27%) | Memory: 47.3 MB (20.92%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,3 +1,4 @@
+/* 
 class Solution {
     public int numDistinct(String s, String t) {
         int m=s.length();
@@ -22,5 +23,34 @@ class Solution {
             return dp[i][j] = helper(s,t,i-1,j-1,dp)+helper(s,t,i-1,j,dp);
         }
         return dp[i][j] = helper(s,t,i-1,j,dp);
+    }
+}
+*/
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0){
+                    dp[i][j]=0;
+                }
+                if(j==0){
+                    dp[i][j]=1;
+                }
+            }
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
