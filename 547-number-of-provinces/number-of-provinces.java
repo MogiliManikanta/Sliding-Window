a19f3b2c41ffe16cc288f382f13998fc4fd5bbb0
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-25 04:54:32 +0530

    Time: 3 ms (34.13%) | Memory: 45.5 MB (79.91%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,26 +1,34 @@
 class Solution {
-    public int findCircleNum(int[][] M) {
-        int N = M.length;
-        boolean[]visited = new boolean[N];
-        int count = 0;
-        
-        for(int i = 0; i < N ;i++){
-            if(!visited[i]){
-                count++;
-                dfs(M,i,visited);
+    public int findCircleNum(int[][] matrix) {
+        ArrayList<ArrayList<Integer>>list = new ArrayList<>();
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==1 && i!=j){
+                    list.get(i).add(j);
+                    list.get(j).add(i);
+                }
             }
         }
-      return count;  
+        boolean[] vis = new boolean[n];
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(list,i,vis);
+            }
+        }
+        return cnt;
     }
-    
-    
-    private void dfs(int[][]M,int i,boolean[]visited){
-        for(int j = 0 ; j < M[i].length ; j++){
-            if(!visited[j] && M[i][j] != 0){
-                visited[j] = true;
-                dfs(M,j,visited);
+    void dfs(ArrayList<ArrayList<Integer>>list,int node,boolean[] vis){
+        vis[node]=true;
+        for(Integer it : list.get(node)){
+            if(!vis[it]){
+                dfs(list,it,vis);
             }
         }
     }
-    
 }
