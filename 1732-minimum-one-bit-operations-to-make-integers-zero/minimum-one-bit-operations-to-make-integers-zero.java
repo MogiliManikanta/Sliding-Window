4f5d3a8738d971afef60c972d29d852171a5f44a
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-11-30 06:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (16.83%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+     public int minimumOneBitOperations(int n) {
+        int sign = 1, res = 0;
+        while (n > 0) {
+            res += n ^ (n - 1) * sign;
+            n &= n - 1;
+            sign = -sign;
+        }
+        return Math.abs(res);
+    }
+}
