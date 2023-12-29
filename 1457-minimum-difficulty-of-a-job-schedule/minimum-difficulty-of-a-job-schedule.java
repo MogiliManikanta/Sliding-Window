7162fa15a693df62a9c9179f1afb8fb0cf57c290
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-12-29 18:02:11 +0530

    Time: 8 ms (88.46%) | Memory: 41.3 MB (20.24%) - LeetSync

---
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,25 @@
+class Solution {
+     public int minDifficulty(int[] jobDifficulty, int D) {
+        final int N = jobDifficulty.length;
+        if(N < D) return -1;
+        int[][] dp = new int[D][N];
+
+        dp[0][0] = jobDifficulty[0];
+        for(int j = 1; j < N; ++j){
+            dp[0][j] = Math.max(jobDifficulty[j], dp[0][j - 1]);
+        }
+
+        for(int d = 1; d < D; ++d){
+            for(int len = d; len < N; ++len){
+                int localMax = jobDifficulty[len];
+                dp[d][len] = Integer.MAX_VALUE;
+                for(int schedule = len; schedule >= d; --schedule){
+                    localMax = Math.max(localMax, jobDifficulty[schedule]);
+                    dp[d][len] = Math.min(dp[d][len], dp[d - 1][schedule - 1] + localMax);
+                }
+            }
+        }
+
+        return dp[D - 1][N - 1];
+    }
+}
