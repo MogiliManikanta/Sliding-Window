c0145b8595db2b00ad945daf04eb4b0479c6467a
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-02-14 03:30:47 +0530

    Time: 2 ms (14.84%) | Memory: 41.8 MB (22.74%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,17 +1,27 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//       int result=0;
+//       s=s.trim();
+//       for(int i=s.length()-1;i>=0;i--){
+//           if(s.charAt(i)!=' '){
+//               result++;
+//           }
+//           else{
+//               //if(result>0){
+//                   return result;
+//               //}
+//           }
+//       }
+//       return result;  
+//     }
+// }/**   
+
 class Solution {
     public int lengthOfLastWord(String s) {
-      int result=0;
-      s=s.trim();
-      for(int i=s.length()-1;i>=0;i--){
-          if(s.charAt(i)!=' '){
-              result++;
-          }
-          else{
-              //if(result>0){
-                  return result;
-              //}
-          }
-      }
-      return result;  
+        String[] chars = s.split("[\\s]+");
+        int length = chars[chars.length - 1].length();
+
+        return length;
     }
 }
+ 
