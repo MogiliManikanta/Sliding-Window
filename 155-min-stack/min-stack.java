eeb4a5c8309633f4d4180c258ae7c089ada275cb
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-02-23 17:54:29 +0530

    Time: 4 ms (96.31%) | Memory: 44.9 MB (49.73%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,34 +1,27 @@
 class MinStack {
-	private Node head;
-        
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
     public void push(int x) {
-        if (head == null) 
-            head = new Node(x, x, null);
-        else 
-            head = new Node(x, Math.min(x, head.min), head);
-    }
-    
-    public void pop() {
-        head = head.next;
-    }
-    
-    public int top() {
-        return head.val;
-    }
-    
-    public int getMin() {
-        return head.min;
-    }
-        
-    private class Node {
-        int val;
-        int min;
-        Node next;
-            
-        private Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
+            min=x;
         }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
     }
 }
