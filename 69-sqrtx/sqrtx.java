fbc34d84145921a5b72078c8173d4d849272b596
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-02-24 14:00:49 +0530

    Time: 1 ms (87.49%) | Memory: 40.3 MB (96.46%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,31 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0) return 0;
-        if(x==1) return 1;
-        int left = 0;
-    int right = x;
-    
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        
-        if ((long)mid * mid <= x) {
-            left = mid + 1;
-        } else {
-            right = mid;
+        long low=0;
+        long high=x;
+        //long mid;
+        long result=-1;
+        while(low<=high){
+           long mid=(low+(high-low)/2);
+            if(mid*mid==x){
+                result=mid;
+                return (int)result;
+                //break;
+            }
+            else if(mid*mid<x){
+                result=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-    }
-    
-    return left - 1;
+        return (int)result;
     }
 }
-/**  
-  
-  
-  There's one thing I'd like to point out. Remember I say that we usually look for the minimal k value satisfying certain condition? But in this problem we are searching for maximal k value instead. Feeling confused? Don't be. Actually, the maximal k satisfying condition(k) is False is just equal to the minimal k satisfying condition(k) is True minus one. This is why I mentioned earlier that we need to decide which value to return, left or left - 1
-  
-  
-  
-  
-  
-  
-   */
