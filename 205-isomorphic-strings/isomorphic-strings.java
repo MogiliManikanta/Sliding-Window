dc77806f4a217c4576c334aee3d180dd101a0092
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-13 10:39:23 +0530

    Time: 16 ms (26.63%) | Memory: 41.7 MB (99.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -5,16 +5,20 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             if (map.containsKey(s.charAt(i))) { // If the map already contains the character from string s.
-                if (map.get(s.charAt(i)) != t.charAt(i)) { // If the character mapping does not match the character in string t.
+                if (map.get(s.charAt(i)) != t.charAt(i)) { // If the character mapping does not match the character in
+                                                           // string t.
                     return false; // Return false as the strings are not isomorphic.
                 }
             } else {
-                if (map.containsValue(t.charAt(i))) { // If the character from string t is already mapped to a different character.
+                if (map.containsValue(t.charAt(i))) { // If the character from string t is already mapped to a different
+                                                      // character.
                     return false; // Return false as the strings are not isomorphic.
                 }
             }
-            map.put(s.charAt(i), t.charAt(i)); // Add the character from string s to string t as a mapping in the HashMap.
+            map.put(s.charAt(i), t.charAt(i)); // Add the character from string s to string t as a mapping in the
+                                               // HashMap.
         }
-        return true; // If the loop completes without finding any non-isomorphic mappings, return true.
+        return true; // If the loop completes without finding any non-isomorphic mappings, return
+                     // true.
     }
 }
