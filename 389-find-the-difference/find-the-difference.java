3b4994020c5b558c8b8807a2d3d0e5c1bbd5763c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-14 12:04:42 +0530

    Time: 2 ms (65.57%) | Memory: 41.2 MB (98.34%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,11 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int charCode = t.charAt(s.length());
-        // Iterate through both strings and char codes
+        int charCode = 0;
+        // Iterate through both strings and XOR char codes
         for (int i = 0; i < s.length(); ++i) {
-              charCode -= (int)s.charAt(i);
-              charCode += (int)t.charAt(i); 
+            charCode ^= (int)s.charAt(i);
+            charCode ^= (int)t.charAt(i);
         }
+        // XOR the last character of t
+        charCode ^= (int)t.charAt(t.length() - 1);
         return (char)charCode;
     }
 }
