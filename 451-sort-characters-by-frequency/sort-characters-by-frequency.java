651e1b65a581a76b2590970d0c233a2aa20f3d1b
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-14 13:58:42 +0530

    Time: 13 ms (73.44%) | Memory: 45.5 MB (63.39%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,9 +1,11 @@
 class Solution {
     public String frequencySort(String s) {
+        // Step 1: Count the frequency of each character in the string
         Map<Character,Integer> map = new HashMap<>();
         for(char c : s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
+        // Step 2: Create a bucket array to group characters by their frequencies
         List<Character>[] bucket = new List[s.length()+1];
         for(char key :map.keySet()){
             int frequency = map.get(key);
@@ -12,16 +14,19 @@ class Solution {
             }
             bucket[frequency].add(key);
         }
+        // Step 3: Construct the sorted string by iterating through the bucket array
         StringBuilder sb = new StringBuilder();
         for(int i=bucket.length-1;i>=0;i--){
             if(bucket[i]!=null){
                 for(char c : bucket[i]){
                     for(int j=0;j<i;j++){
-                        sb.append(c);
+                        sb.append(c); // Append the character 'i' times to the result string
                     }
                 }
             }
         }
+        
+        // Step 4: Return the sorted string
         return sb.toString();
     }
 }
