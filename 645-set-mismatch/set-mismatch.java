0b7e41a6b749746f9cf56d6eb3278303b83ec0d7
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-15 09:44:31 +0530

    Time: 11 ms (43.79%) | Memory: 45.7 MB (33.29%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,13 +1,25 @@
+import java.util.*;
 class Solution {
-    public static int[] findErrorNums(int[] nums) {
-    int[] res = new int[2];
-    for (int i : nums) {
-        if (nums[Math.abs(i) - 1] < 0) res[0] = Math.abs(i);
-	else nums[Math.abs(i) - 1] *= -1;
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int actualSum = 0;
+        int uniqueSum = 0;
+        Set<Integer> numSet = new HashSet<>();
+        
+        for (int num : nums) {
+            actualSum += num;
+            numSet.add(num);
+        }
+        
+        for (int num : numSet) {
+            uniqueSum += num;
+        }
+        
+        int expectedSum = n * (n + 1) / 2;
+        
+        int duplicate = actualSum - uniqueSum;
+        int missing = expectedSum - uniqueSum;
+        
+        return new int[]{duplicate, missing};
     }
-    for (int i=0;i<nums.length;i++) {
-        if (nums[i] > 0) res[1] = i+1;
-    }
-    return res;
-}
 }
