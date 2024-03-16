f2930fffb3ae9501e00909d7dbd0865cb6c01115
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-16 10:56:55 +0530

    Time: 1 ms (99.30%) | Memory: 41.7 MB (69.34%) - LeetSync

---
+++ 827-expressive-words/expressive-words.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int expressiveWords(String S, String[] words) {
+        if (S == null || words == null) {
+            return 0;
+        }
+        int count = 0;
+        for (String word : words) {
+            if (stretchy(S, word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public boolean stretchy(String S, String word) {
+        if (word == null) {
+            return false;
+        }
+        int i = 0;
+        int j = 0;
+        while (i < S.length() && j < word.length()) {
+            if (S.charAt(i) == word.charAt(j)) {
+                int len1 = getRepeatedLength(S, i);
+                int len2 = getRepeatedLength(word, j);
+                if (len1 < 3 && len1 != len2 || len1 >= 3 && len1 < len2) {
+                    return false;
+                }
+                i += len1;
+                j += len2;
+            } else {
+                return false;
+            }
+        }
+        return i == S.length() && j == word.length();
+    }
+    
+    public int getRepeatedLength(String str, int i) {
+        int j = i;
+        while (j < str.length() && str.charAt(j) == str.charAt(i)) {
+            j++;
+        }
+        return j - i;
+    }
+}
