278920d081db52ec5b45852b7f70aa963ad9d276
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-16 11:22:47 +0530

    Time: 1 ms (99.30%) | Memory: 41.7 MB (69.34%) - LeetSync

--- 827-expressive-words/expressive-words.java
+++ 827-expressive-words/expressive-words.java
@@ -1,44 +1,41 @@
 class Solution {
-    public int expressiveWords(String S, String[] words) {
-        if (S == null || words == null) {
+    public int expressiveWords(String s, String[] words) {
+        if(s==null || words==null){
             return 0;
         }
-        int count = 0;
-        for (String word : words) {
-            if (stretchy(S, word)) {
+        int count=0;
+        for(String S : words){
+            if(strechy(s,S)){
                 count++;
             }
         }
         return count;
     }
-    
-    public boolean stretchy(String S, String word) {
-        if (word == null) {
+    public boolean strechy(String s,String word){
+        if(s==null || word==null){
             return false;
         }
-        int i = 0;
-        int j = 0;
-        while (i < S.length() && j < word.length()) {
-            if (S.charAt(i) == word.charAt(j)) {
-                int len1 = getRepeatedLength(S, i);
-                int len2 = getRepeatedLength(word, j);
-                if (len1 < 3 && len1 != len2 || len1 >= 3 && len1 < len2) {
+        int i=0,j=0;
+        while(i<s.length() && j<word.length()){
+            if(s.charAt(i)==word.charAt(j)){
+                int len1 = getRepeatedLength(s,i);
+                int len2 = getRepeatedLength(word,j);
+                if((len1<3 && len1!=len2) || (len1>=3 &&len1<len2)){
                     return false;
                 }
-                i += len1;
-                j += len2;
-            } else {
+                i+=len1;
+                j+=len2;
+            }else{
                 return false;
             }
         }
-        return i == S.length() && j == word.length();
+        return i==s.length() && j==word.length();
     }
-    
-    public int getRepeatedLength(String str, int i) {
-        int j = i;
-        while (j < str.length() && str.charAt(j) == str.charAt(i)) {
+    public int getRepeatedLength(String s,int i){
+        int j=i;
+        while(j<s.length() && s.charAt(j)==s.charAt(i)){
             j++;
         }
-        return j - i;
+        return j-i;
     }
 }
