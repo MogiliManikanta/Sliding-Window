517995c052bb9fab7dd93db5defbc6bfc10a0a9b
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-18 12:36:56 +0530

    Time: 8 ms (56.03%) | Memory: 45.2 MB (47.28%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,33 +1,30 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
         int[] counts = new int[26];
-        // Step 1: Initialize Character Counts Array
-        for(int i=0;i<chars.length();i++)
-        {
-            counts[chars.charAt(i)-'a']++;
+        for (Character c : chars.toCharArray()) {
+            counts[c - 'a']++;
         }
-        int res =0 ;
-        // Step 3: Check Words
-        for(String s : words)
-        {
-            if(canForm(s,counts))
-                // Step 4: Calculate Lengths
-                res+=s.length();
+        
+        int ans = 0;
+        for (String word : words) {
+            int[] wordCount = new int[26];
+            for (Character c : word.toCharArray()) {
+                wordCount[c - 'a']++;
+            }
+            
+            boolean good = true;
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] < wordCount[i]) {
+                    good = false;
+                    break;
+                }
+            }
+            
+            if (good) {
+                ans += word.length();
+            }
         }
-        return res;
-    }
-
-    boolean canForm(String word, int[] counts)
-    {
-        int[] c = new int[26];
-        // Step 2: Update Counts Array
-        for(int i =0;i<word.length();i++)
-        {
-            int x = word.charAt(i)-'a';
-            c[x]++;
-            if(c[x] > counts[x])
-                return false;
-        }
-        return true;
+        
+        return ans;
     }
 }
