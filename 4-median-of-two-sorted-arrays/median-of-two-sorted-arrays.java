e1c3257acbdf8a249e11c042c5aeab54433cd194
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-03-28 10:25:48 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (87.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,40 +1,28 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        if(m>n){
-            int []temp=nums1;
-            nums1=nums2;
-            nums2=temp;
-            int templength=m;
-            m=n;
-            n=templength;
+    public double findMedianSortedArrays(int[] a, int[] b) {
+         int n1 = a.length, n2 = b.length;
+        //if n1 is bigger swap the arrays:
+        if (n1 > n2) return findMedianSortedArrays(b, a);
+
+        int n = n1 + n2; //total length
+        int left = (n1 + n2 + 1) / 2; //length of left half
+        //apply binary search:
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            //calculate l1, l2, r1 and r2;
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-        int low=0;
-        int high=m;
-        int middlePosition=((n+m)+1)/2;
-        while(low<=high){
-            int cut1=(low+high)/2;
-            int cut2=middlePosition-cut1;
-            int l1=(cut1==0) ? Integer.MIN_VALUE: nums1[cut1-1];
-            int l2=(cut2==0) ? Integer.MIN_VALUE: nums2[cut2-1];
-            int r1=(cut1==m) ?  Integer.MAX_VALUE: nums1[cut1];
-            int r2=(cut2==n) ?  Integer.MAX_VALUE: nums2[cut2];
-            if((l1<=r2) && (l2<=r1)){
-                if((m+n)%2==0){
-                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
-                }
-                else{
-                    return Math.max(l1,l2);
-                }
-            }
-            else if(l1>r2){
-                high=cut1-1;
-            }
-            else{
-                low=cut1+1;
-            }
-        }
-        return 0.0;
+        return 0; //dummy statement
     }
 }
