9b2e0d77dcf0eb58e19c4e31001d5bce3203be08
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-04-02 08:52:40 +0530

    Time: 2 ms (85.77%) | Memory: 42.7 MB (8.35%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,9 +1,8 @@
-class Solution {
+/*class Solution {
     public List<Integer> generateRow(int row) {
         long ans = 1;
         List<Integer> ansRow = new ArrayList<>();
         ansRow.add(1); //inserting the 1st element
-
         //calculate the rest of the elements:
         for (int col = 1; col < row; col++) {
             ans = ans * (row - col);
@@ -14,8 +13,44 @@ class Solution {
     }
     public List<List<Integer>> generate(int n) {
         List<List<Integer>> ans = new ArrayList<>();
-
         //store the entire pascal's triangle:
+        for (int row = 1; row <= n; row++) {
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+}*/
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> generateRow(int n) {
+        ArrayList<Long> pre = new ArrayList<>();
+        long mod = 1000000007;
+        for (int i = 0; i < n; i++) {
+            ArrayList<Long> current = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                if (j == i | j == 0) {
+                    current.add(1l);
+                } else {
+                    long num = (pre.get(j) + pre.get(j - 1)) % mod;
+                    current.add(num);
+                }
+            }
+            pre = current;
+        }
+        
+        // Convert ArrayList<Long> to List<Integer>
+        List<Integer> result = new ArrayList<>();
+        for (Long num : pre) {
+            result.add(num.intValue());
+        }
+        return result;
+    }
+
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        // Store the entire Pascal's triangle:
         for (int row = 1; row <= n; row++) {
             ans.add(generateRow(row));
         }
