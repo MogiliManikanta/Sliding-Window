3d11b3460051aafd5efd85ac989b318805c97c62
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-05-11 09:18:37 +0530

    Time: 30 ms (47.43%) | Memory: 45.7 MB (63.64%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
@@ -1,64 +1,17 @@
-//  Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
-// -> group of workers share the same "cost[i]  / quality[i]", let's call it PAID_RATIO
-// -> PAID_RATIO = total cost / total quality
-
-// Every worker in the paid group must be paid at least their minimum wage expectation.
-// -> cost[i] >= wage[i]
-// -> cost[i] / quality[i] >= wage[i] / quality[i]
-// -> PAID_RATIO >= MAX RATIO(maximum wage[i] / quality[i] in the group)
-
-// cost = groupQuality * PAID_RATIO
-// -> groupQuality * MAX RATIO(maximum wage[i] / quality[i] in the group) 
-// -> minCost = Math.min(minCost, minimum groupQuality * minimum MAX RATIO)
-
-// get minimum groupQuality -> pop highest quality when size is bigger than k -> maxHeap 
-// track minimum MAX RATIO -> keep current ratio -> increse order workers list
-class Solution{
-public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
-    List<Worker> workers = new ArrayList<>();
-
-    for (int i = 0; i < quality.length; i++) {
-        workers.add(new Worker(quality[i], wage[i]));
-    }
-
-    // Sort by ratio increasingly
-    Collections.sort(workers, (a, b) -> Double.compare(a.ratio, b.ratio));
-
-    // Maxheap always pop highest quality
-    PriorityQueue<Worker> maxHeap = new PriorityQueue<>((a, b) -> (b.quality - a.quality));
-
-    int totalQuality = 0;
-    double minCost = Double.MAX_VALUE;
-    for (Worker worker: workers) {
-        maxHeap.offer(worker);
-        totalQuality += worker.quality;
-
-        if (maxHeap.size() > K) {
-            // pop highest quality worker
-            Worker removedWorker = maxHeap.poll();
-            totalQuality -= removedWorker.quality;
+class Solution {
+        public double mincostToHireWorkers(int[] q, int[] w, int K) {
+        double[][] workers = new double[q.length][2];
+        for (int i = 0; i < q.length; ++i)
+            workers[i] = new double[]{(double)(w[i]) / q[i], (double)q[i]};
+        Arrays.sort(workers, (a, b) -> Double.compare(a[0], b[0]));
+        double res = Double.MAX_VALUE, qsum = 0;
+        PriorityQueue<Double> pq = new PriorityQueue<>();
+        for (double[] worker: workers) {
+            qsum += worker[1];
+            pq.add(-worker[1]);
+            if (pq.size() > K) qsum += pq.poll();
+            if (pq.size() == K) res = Math.min(res, qsum * worker[0]);
         }
-
-        if (maxHeap.size() == K) {
-            // groupQuality * MAX RATIO(maximum wage[i] / quality[i] in the group) 
-            minCost = Math.min(totalQuality * worker.ratio, minCost);
-        }
-    }
-
-    return minCost;
-}
-// time: O(nlogn) - sort and for loop 
-// space: O(n) - List
-}
-class Worker {
-    int quality;
-    int wage;
-    double ratio;
-    
-    public Worker(int quality, int wage) {
-        this.quality = quality;
-        this.wage = wage;
-        // be careful about int = int / int, double = double / int
-        ratio = wage * 1.0 / quality;
+        return res;
     }
 }
