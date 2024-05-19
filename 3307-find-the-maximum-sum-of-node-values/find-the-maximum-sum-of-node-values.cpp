5b25ec92ac75458102a8f63e1e1e10252cdb71bd
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-05-19 16:53:03 +0530

    Time: 176 ms (88.44%) | Memory: 125.8 MB (48.89%) - LeetSync

---
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -0,0 +1,14 @@
+#define ll long long
+#define XOR(val) ((ll)val ^ k)
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+    long long sum = 0, cnt = 0, sacrifice = INT_MAX;
+    for (long long n : nums) {
+        sum += max(n ^ k, n);
+        cnt += (n ^ k) > n;
+        sacrifice = min(sacrifice, abs(n - (n ^ k)));
+    }
+    return sum - (cnt % 2 ? sacrifice : 0);
+}
+};
