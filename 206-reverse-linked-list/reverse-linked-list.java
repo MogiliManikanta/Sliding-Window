5919d5c2d577cf7f9847e1c5992237d2954ea55a
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-07-17 08:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (55.34%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -22,13 +22,32 @@ class Solution {
         // return prev;
 
 
-        if(head==null || head.next==null){
+        // Base case:
+        // If the linked list is empty or has only one node,
+        // return the head as it is already reversed.
+        if (head == null || head.next == null) {
             return head;
         }
-
-        ListNode reverse = reverseList(head.next);
-        head.next.next=head;
-        head.next=null;
-        return reverse;
+        
+        // Recursive step:
+        // Reverse the linked list starting
+        // from the second node (head.next).
+        ListNode newHead = reverseList(head.next);
+        
+        // Save a reference to the node following
+        // the current 'head' node.
+        ListNode front = head.next;
+        
+        // Make the 'front' node point to the current
+        // 'head' node in the reversed order.
+        front.next = head;
+        
+        // Break the link from the current 'head' node
+        // to the 'front' node to avoid cycles.
+        head.next = null;
+        
+        // Return the 'newHead,' which is the new
+        // head of the reversed linked list.
+        return newHead;
     }
 }
