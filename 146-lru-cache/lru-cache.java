dad1f985ba8b5da2c4e91e29261ed3cbe7179668
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-07-20 18:46:57 +0530

    Time: 54 ms (22.44%) | Memory: 126.8 MB (8.22%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,101 +1,61 @@
-import java.util.Hashtable;
+class LRUCache {
+    Node head = new Node(0, 0), tail = new Node(0, 0);
+    Map < Integer, Node > map = new HashMap();
+    int capacity;
 
-public class LRUCache {
-
-    // Definition of a doubly-linked list node
-    class DLinkedNode {
-        int key;
-        int value;
-        DLinkedNode pre;
-        DLinkedNode post;
-    }
-
-    // Add a node right after the head
-    private void addNode(DLinkedNode node) {
-        node.pre = head;
-        node.post = head.post;
-        head.post.pre = node;
-        head.post = node;
-    }
-
-    // Remove an existing node from the linked list
-    private void removeNode(DLinkedNode node) {
-        DLinkedNode pre = node.pre;
-        DLinkedNode post = node.post;
-        pre.post = post;
-        post.pre = pre;
-    }
-
-    // Move a certain node in between the head and its original next node
-    private void moveToHead(DLinkedNode node) {
-        this.removeNode(node);
-        this.addNode(node);
-    }
-
-    // Pop the current tail (the least recently used node)
-    private DLinkedNode popTail() {
-        DLinkedNode res = tail.pre;
-        this.removeNode(res);
-        return res;
-    }
-
-    // Hashtable to store key-value pairs for quick access
-    private Hashtable<Integer, DLinkedNode> cache = new Hashtable<Integer, DLinkedNode>();
-    private int count; // Current number of elements in the cache
-    private int capacity; // Maximum capacity of the cache
-    private DLinkedNode head, tail; // Dummy head and tail nodes for the doubly-linked list
-
-    public LRUCache(int capacity) {
-        this.count = 0;
-        this.capacity = capacity;
-
-        // Initialize dummy head and tail nodes for the doubly-linked list
-        head = new DLinkedNode();
-        head.pre = null;
-
-        tail = new DLinkedNode();
-        tail.post = null;
-
-        head.post = tail;
-        tail.pre = head;
+    public LRUCache(int _capacity) {
+        capacity = _capacity;
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
-        DLinkedNode node = cache.get(key);
-        if (node == null) {
-            return -1; // Key not found, should raise an exception here.
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else {
+            return -1;
         }
-
-        // Move the accessed node to the head to indicate it's the most recently used
-        this.moveToHead(node);
-
-        return node.value;
     }
 
     public void put(int key, int value) {
-        DLinkedNode node = cache.get(key);
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
 
-        if (node == null) {
-            DLinkedNode newNode = new DLinkedNode();
-            newNode.key = key;
-            newNode.value = value;
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
 
-            // Add the new node to the cache and the doubly-linked list
-            this.cache.put(key, newNode);
-            this.addNode(newNode);
+    private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
 
-            ++count;
-
-            if (count > capacity) {
-                // If the cache capacity is exceeded, remove the least recently used node
-                DLinkedNode tail = this.popTail();
-                this.cache.remove(tail.key);
-                --count;
-            }
-        } else {
-            // If the key already exists in the cache, update its value and move it to the head
-            node.value = value;
-            this.moveToHead(node);
+    class Node {
+        Node prev, next;
+        int key, value;
+        Node(int _key, int _value) {
+            key = _key;
+            value = _value;
         }
     }
 }
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
