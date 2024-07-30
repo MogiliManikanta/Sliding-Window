c6dcbbca7fecabcaf012c59df649f0d8acd3db9f
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-07-30 17:22:17 +0530

    Time: 9 ms (54.11%) | Memory: 46.7 MB (23.78%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> result = new ArrayList<>();
-        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-
-        for(int[] i : intervals){
-            if(i[0] <= end){ // overlap
-                end = Math.max(end,i[1]);
-            }
-            else{ // no overlap
-                result.add(new int[]{start,end});
-                start=i[0];
-                end=i[1];
+        // Sorting intervals by their starting value
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || interval[0] > merged.get(merged.size() - 1)[1]) {
+                merged.add(interval);
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
             }
         }
-        result.add(new int[]{start,end});
-        return result.toArray(new int[result.size()][2]);
+        
+        return merged.toArray(new int[merged.size()][]);
     }
 }
