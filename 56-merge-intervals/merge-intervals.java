cdb942bd23c627759c69aeee669aa44a4206decb
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-07-30 17:41:03 +0530

    Time: 10 ms (29.45%) | Memory: 46.1 MB (94.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -6,15 +6,18 @@ class Solution {
                 return a[0] - b[0];
             }
         });
-        
         List<int[]> merged = new ArrayList<>();
         for (int[] interval : intervals) {
-            if (merged.isEmpty() || interval[0] > merged.get(merged.size() - 1)[1]) {
-                merged.add(interval);
-            } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
-            }
-        }
+    // If merged list is empty or current interval doesn't overlap with the last one in merged
+    if (merged.isEmpty() || interval[0] > merged.get(merged.size() - 1)[1]) {
+        // Add current interval to merged list
+        merged.add(interval);
+    } else {
+        // There is an overlap, merge the intervals by extending the end of the last interval in merged
+        merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+    }
+}
+
         
         return merged.toArray(new int[merged.size()][]);
     }
