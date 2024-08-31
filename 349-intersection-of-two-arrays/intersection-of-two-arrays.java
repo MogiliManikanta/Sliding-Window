ae6564dec6be9d3f0360fb359ad565b7047a7d24
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-08-31 14:34:45 +0530

    Time: 2 ms (95.39%) | Memory: 43.2 MB (61.68%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,34 +1,19 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        // Create a HashSet to store unique elements from nums1
-        HashSet<Integer> set1 = new HashSet<>();
-        
-        // Iterate through nums1 and add each element to set1
-        for (int i : nums1) {
-            set1.add(i);
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums1){
+            set.add(i);
         }
-        
-        // Create another HashSet to store common elements between nums1 and nums2
-        HashSet<Integer> set2 = new HashSet<>();
-        
-        // Iterate through nums2
-        for (int i : nums2) {
-            // If the element 'i' exists in set1, it's a common element, so add it to set2
-            if (set1.contains(i)) {
+        HashSet<Integer>set2=new HashSet<>();
+        for(int i : nums2){
+            if(set.contains(i)){
                 set2.add(i);
             }
         }
-        
-        // Create an array to store the intersection elements
         int[] result = new int[set2.size()];
-        int index = 0;
-        
-        // Iterate through set2 to fill the result array
-        for (Integer i : set2) {
-            result[index++] = i;
-        }
-        
-        // Return the array containing the intersection of nums1 and nums2
-        return result;
+        int index=0;
+        for(Integer i : set2){
+            result[index++]=i;
+        }return result;
     }
 }
