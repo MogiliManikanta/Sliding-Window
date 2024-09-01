e3a0d960c022170966b9dc4c6e4a12d25dc5a83c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-01 22:59:43 +0530

    Time: 2 ms (71.31%) | Memory: 61.5 MB (61.78%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -4,7 +4,7 @@ class Solution {
        int mini = Integer.MAX_VALUE;
        int maxi=Integer.MIN_VALUE;
        for(int i=0;i<n;i++){
-        mini=Math.min(mini,prices[i]);
+         mini=Math.min(mini,prices[i]);
          int temp=prices[i]-mini;
          maxi=Math.max(maxi,temp);
        }
