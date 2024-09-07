332954f4698b6a1e0fcb3fa34dc9441ddc641fba
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-07 22:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (32.60%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,47 +1,57 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int col0 = 1;
-
-        // Step 1: Mark rows and columns
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    // Mark the i-th row
-                    matrix[i][0] = 0;
-
-                    // Mark the j-th column
-                    if (j != 0) {
-                        matrix[0][j] = 0;
-                    } else {
-                        col0 = 0;
-                    }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] rowAns = new int[row];
+        int[] colAns = new int[col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    rowAns[i]=-1;
+                    colAns[j]=-1;
                 }
             }
         }
-
-        // Step 2: Use the marks to set zeros in the matrix
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] != 0) {
-                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                        matrix[i][j] = 0;
-                    }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rowAns[i]==-1 || colAns[j]==-1){
+                    matrix[i][j]=0;
                 }
-            }
-        }
-
-        // Step 3: Handle the first row and first column separately
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if (col0 == 0) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
             }
         }
     }
 }
+/*****class Solution {
+    public void setZeroes(int[][] matrix) {
+        int col=matrix[0].length;
+        int row=matrix.length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    markRow(matrix,i);
+                    markCol(matrix,j);
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        // return matrix;
+    }
+    public void markRow(int[][] matrix,int row){
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[row][i]!=0)
+                matrix[row][i]=-1;
+        }
+    }
+    public void markCol(int[][] matrix,int col){
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][col]!=0)
+                matrix[i][col]=-1;
+        }
+    }
+} */
