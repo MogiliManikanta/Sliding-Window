20ec0e30eb5b96b00315456d1129277c0275d873
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-07 23:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (52.60%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,5 +1,44 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int col0=1,row=matrix.length,col=matrix[0].length;
+        // int col[m] = matrix[...][0]
+        // int row[n] = matrix[0][...]
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    // mark the row 
+                    matrix[i][0]=0;
+                    // mark the col
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        // For the column
+        if(matrix[0][0]==0){
+            for(int i=0;i<col;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<row;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
+/*class Solution {
+    public void setZeroes(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
         int[] rowAns = new int[row];
@@ -20,7 +59,7 @@ class Solution {
             }
         }
     }
-}
+}*/
 /*****class Solution {
     public void setZeroes(int[][] matrix) {
         int col=matrix[0].length;
