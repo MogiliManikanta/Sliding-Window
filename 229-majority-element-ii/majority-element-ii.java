f9c3621add58e09d600fa687b5fd31986a0fbec6
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-08 17:25:22 +0530

    Time: 2 ms (99.47%) | Memory: 48 MB (51.72%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,37 @@
-class Solution {
+public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        int cnt1=0,cnt2=0;
-        int ele1=nums[0],ele2=nums[0],n=nums.length;
-        for(int i=0;i<n;i++){
-            if(cnt1==0 && nums[i]!=ele2){
-                ele1=nums[i];
-                cnt1++;
-            }
-            else if(cnt2==0 && nums[i]!=ele1){
-                ele2=nums[i];
-                cnt2++;
-            }
-            else if(ele1==nums[i]){
-                cnt1++;
-            }
-            else if(ele2==nums[i]){
-                cnt2++;
-            }else{
-                cnt1--;
-                cnt2--;
-            }
-        }
-        int cnt12=0,cnt22=0;
-        int req=(n/3)+(n%3);
-        for(int i=0;i<n;i++){
-            if(ele1==nums[i]) cnt12++;
-            else if(ele2==nums[i]) cnt22++;
-        }
-        if(cnt12> n/3){
-                list.add(ele1);
-            }
-            if(cnt22>n/3){
-                list.add(ele2);
-            }
-        return list;
-    }
+	if (nums == null || nums.length == 0)
+		return new ArrayList<Integer>();
+	List<Integer> result = new ArrayList<Integer>();
+	int number1 = nums[0], number2 = nums[0], count1 = 0, count2 = 0, len = nums.length;
+	for (int i = 0; i < len; i++) {
+		if (nums[i] == number1)
+			count1++;
+		else if (nums[i] == number2)
+			count2++;
+		else if (count1 == 0) {
+			number1 = nums[i];
+			count1 = 1;
+		} else if (count2 == 0) {
+			number2 = nums[i];
+			count2 = 1;
+		} else {
+			count1--;
+			count2--;
+		}
+	}
+	count1 = 0;
+	count2 = 0;
+	for (int i = 0; i < len; i++) {
+		if (nums[i] == number1)
+			count1++;
+		else if (nums[i] == number2)
+			count2++;
+	}
+	if (count1 > len / 3)
+		result.add(number1);
+	if (count2 > len / 3)
+		result.add(number2);
+	return result;
+}
 }
