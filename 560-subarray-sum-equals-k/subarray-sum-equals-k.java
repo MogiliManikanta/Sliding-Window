29e456bc4b4a85d73f1b22c741e8ad4b57f27224
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-08 12:40:50 +0530

    Time: 24 ms (72.68%) | Memory: 46.8 MB (15.85%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,27 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        int sum = 0, ans = 0;
-        mp.put(sum, 1);
-        for (int num : nums) {
-            sum += num;
-            int find = sum - k;
-            if (mp.containsKey(find)) {
-                ans += mp.get(find);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int sum=0;
+        int n=nums.length;
+        map.put(sum,1);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            int remaining = sum-k;
+            if(map.containsKey(remaining)){
+                cnt+=map.get(remaining);
             }
-            mp.put(sum, mp.getOrDefault(sum, 0) + 1);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        return ans;
-    } 
+        return cnt;
+    }
 }
-/**any idea guys, why do we have to initialize map like preSum.put(0, 1); .........I mean how would we know to do this if approaching this solution for first time? I got most of the things in for loop correct (ofc i had seen "two sum" problem, "Maximum Size Subarray Sum Equals k" under amazon problem before and used similar ideas here...)...still don't get why we initialize the map...
-
-##################
-
-I see ...After spending some time on the analysis, I found the reason behind having initialize preSum.put(0,1)....it is for those (sum - k) == 0 calculations which are valid subarrays but need to get counted. e.g. if k = 7 and sum = 7 (at second element for array is : 3, 4, 3, 8) at some iteration.....then sum - k = 0....this 0 will get counted in statement result += preSum.get(sum - k);
-
-#############
-
-So in conclusion, the initial entry preSum.put(0, 1) can be exchanged with statement :
-if (sum == k) count++;
-we can put it just below sum += nums[i]; statement. This will make code more intuitive...as we are trying to find the sum which matches to k! */
