5c9fbb2e895e8373355f6eee957bb0eb9f5e8786
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-08 11:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (91.54%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -15,7 +15,7 @@ class Solution {
         // 1 2 3        1 4 7        7 4 1
         // 4 5 6    ==> 2 5 8   ==>  8 5 2
         // 7 8 9        3 6 9        9 6 3
-
+        // swap the columns in place of matrix using in place algorithm
         for(int i=0;i<row;i++){
             for(int j=0;j<column/2;j++){
                 int temp = matrix[i][j];
