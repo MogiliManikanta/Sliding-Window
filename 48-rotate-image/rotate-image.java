6203fdeb85618fa9a5238f6a84ba6a642682cec6
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-08 11:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.31%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,38 +1,27 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int row = matrix.length;
-        int column = matrix[0].length;
+        int row=matrix.length;
+        int column=matrix[0].length;
+        // step 1 : Transpose the matrix
         for(int i=0;i<row;i++){
-            for(int j=i;j<column;j++){
-                int temp=matrix[i][j];
+            for(int j=i+1;j<column;j++){
+                // swap the matrix[i][j] to matrix[j][i]
+                int temp = matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
 
+        // 1 2 3        1 4 7        7 4 1
+        // 4 5 6    ==> 2 5 8   ==>  8 5 2
+        // 7 8 9        3 6 9        9 6 3
+
         for(int i=0;i<row;i++){
             for(int j=0;j<column/2;j++){
                 int temp = matrix[i][j];
-                matrix[i][j]=matrix[i][column-1-j];  // in place algorithm
+                matrix[i][j] = matrix[i][column-1-j];
                 matrix[i][column-1-j]=temp;
             }
         }
     }
 }
-/**done once see it
-1.step1 transpose
-2.reverse using in place algorithm
-
-/** step1 : transpose
-1 4 7
-2 5 8
-3 6 9
-
-
-step 2 : in-place algorithm
-
-7 4 1 
-8 5 2
-9 6 3
-
-*/
