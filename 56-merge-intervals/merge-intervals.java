4c4bc6fac3dde2ddccabe32fa650b8cb4819634e
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-11 18:55:41 +0530

    Time: 10 ms (26.17%) | Memory: 46.1 MB (93.25%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Sorting intervals by their starting value
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[]a ,int[]b){
+                return a[0]-b[0];
             }
         });
-        // Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-        List<int[]> merged = new ArrayList<>();
-        for (int[] interval : intervals) {
-    // If merged list is empty or current interval doesn't overlap with the last one in merged
-    if (merged.isEmpty() || interval[0] > merged.get(merged.size() - 1)[1]) {
-        // Add current interval to merged list
-        merged.add(interval);
-    } else {
-        // There is an overlap, merge the intervals by extending the end of the last interval in merged
-        merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
-    }
-}
 
-        
-        return merged.toArray(new int[merged.size()][]);
+        // Arrays.sort(intervals, (a,b)-> Integer.compareTo(a,b));
+        List<int[]> result = new ArrayList<>();
+        for(int[] interval : intervals){
+            int start = interval[0];
+            int end = interval[1];
+            if(result.isEmpty() || result.get(result.size()-1)[1]<start){
+                result.add(interval);
+            }else{
+                result.get(result.size()-1)[1] = Math.max(result.get(result.size()-1)[1],end);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
     }
 }
