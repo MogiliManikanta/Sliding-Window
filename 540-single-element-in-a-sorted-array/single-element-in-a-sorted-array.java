4ce655604be1f5995bc278c708647a76801c77ed
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-16 22:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (42.21%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,20 +1,67 @@
-class Solution {
+public class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n=nums.length;
-        if(nums.length==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        int low=1,high=nums.length-2;
-        while(low<=high){
-            int mid=(low+(high-low)/2);
-            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
-            else if((mid%2==0 && nums[mid+1]==nums[mid]) || (mid%2==1 && nums[mid-1] == nums[mid])){
-                low=mid+1;
+        int n = nums.length;
+
+        // Edge cases:
+        if (n == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // If nums[mid] is the single element:
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
             }
-            else{
-                high=mid-1;
-            }  
+
+            // We are in the left:
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
+                    || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                // Eliminate the left half:
+                low = mid + 1;
+            }
+            // We are in the right:
+            else {
+                // Eliminate the right half:
+                high = mid - 1;
+            }
         }
+
+        // Dummy return statement:
         return -1;
     }
 }
+/**EXPLANATION:-
+Suppose array is [1, 1, 2, 2, 3, 3, 4, 5, 5]
+we can observe that for each pair, 
+first element takes even position and second element takes odd position
+for example, 1 is appeared as a pair,
+so it takes 0 and 1 positions. similarly for all the pairs also.
+
+this pattern will be missed when single element is appeared in the array.
+
+From these points, we can implement algorithm.
+1. Take left and right pointers . 
+    left points to start of list. right points to end of the list.
+2. find mid.
+    if mid is even, then it's duplicate should be in next index.
+	or if mid is odd, then it's duplicate  should be in previous index.
+	check these two conditions, 
+	if any of the conditions is satisfied,
+	then pattern is not missed, 
+	so check in next half of the array. i.e, left = mid + 1
+	if condition is not satisfied, then the pattern is missed.
+	so, single number must be before mid.
+	so, update end to mid.
+3. At last return the nums[left]
+
+Time: -  O(logN)
+space:-  O(1)
+
+IF YOU  HAVE ANY DOUBTS, FEEL FREE TO ASK
+IF YOU UNDERSTAND, DON'T FORGET TO UPVOTE. */
