cdb59bdd0ebeecb1c4d30dfad8b2f08b5b9285e1
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-17 22:46:12 +0530

    Time: 25 ms (50.14%) | Memory: 45.5 MB (6.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,36 +1,34 @@
 class Solution {
-    public int maxHelperFunction(int[] arr,int h){
-        int n=arr.length;
-        int maxi=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            maxi=Math.max(arr[i],maxi);
+    public  int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;;
+        int n = v.length;
+        //find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
         }
         return maxi;
     }
-    public int helperFunction(int mid,int[] piles,int hours){
-        int ans=0,n=piles.length;
-        for(int i=0;i<n;i++){
-            // ans += Math.ceil((double)piles[i]/(double)mid);
-            ans+=(piles[i]+mid-1)/mid;
-            if(ans>hours) return -1;
+    public int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        int n = v.length;
+        //find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(v[i]) / (double)(hourly));
         }
-        return 1;
-    } 
-    public int minEatingSpeed(int[] piles, int h) {
-        int low=1;
-         int ans=0;
-        int high = maxHelperFunction(piles,h);
-        while(low<=high){
-           
-            int mid = (low+(high-low)/2);
-            int temp = helperFunction(mid,piles,h);
-            if(temp==1){
-                ans=mid;
-            high=mid-1;
-            }else{
-                low=mid+1;
+        return totalH;
+    }
+    public  int minEatingSpeed(int[] v, int h) {
+        int low = 1, high = findMax(v);
+        //apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(v, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
 }
