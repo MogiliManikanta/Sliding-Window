9b1ff282874d31c9b245cf8e8101bf5175588c8d
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-18 21:34:13 +0530

    Time: 8 ms (87.60%) | Memory: 48.3 MB (27.58%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,33 +1,29 @@
 class Solution {
-    public int smallestDivisor(int[] arr, int limit) {
-        int n = arr.length; //size of array.
-        if(n > limit) return -1;
-        //Find the maximum element:
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, arr[i]);
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low=Integer.MAX_VALUE;
+        int high=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            low=Math.min(low,nums[i]);
+            high=Math.max(high,nums[i]);
         }
-
-        int low = 1, high = maxi;
-
-        //Apply binary search:
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (sumByD(arr, mid) <= limit) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int ans=-1;
+        low=1;
+        while(low<=high){
+            int mid=(low+(high-low)/2);
+            if(possible(mid,threshold,nums)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-        return low;
+        return ans;
     }
-    public int sumByD(int[] arr, int div) {
-        int n = arr.length; //size of array
-        //Find the summation of division values:
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil((double)(arr[i]) / (double)(div));
+    public boolean possible(int mid,int limit,int[] nums){
+    long sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=(nums[i]+mid-1)/mid;
         }
-        return sum;
+        return sum<=limit;
     }
 }
