414c0d39f0e65324113e05eaf359b8b599a0117c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-20 23:29:12 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (81.87%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,28 +1,29 @@
 class Solution {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-         int n1 = a.length, n2 = b.length;
-        //if n1 is bigger swap the arrays:
-        if (n1 > n2) return findMedianSortedArrays(b, a);
-
-        int n = n1 + n2; //total length
-        int left = (n1 + n2 + 1) / 2; //length of left half
-        //apply binary search:
-        int low = 0, high = n1;
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
-            //calculate l1, l2, r1 and r2;
-            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
-            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
-            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
-
-            if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) return Math.max(l1, l2);
-                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
-            } else if (l1 > r2) high = mid1 - 1;
-            else low = mid1 + 1;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int low=0;
+        int high=n1;
+        int left=(n1+n2+1)/2;
+        while(low<=high){
+            int mid1=(low+high)/2;
+            int mid2=(left-mid1);
+            int leftElement1 = mid1-1>=0?nums1[mid1-1] : Integer.MIN_VALUE;
+            int leftElement2 = mid2-1>=0?nums2[mid2-1] : Integer.MIN_VALUE;
+            int rightElement1 = mid1<n1?nums1[mid1] :Integer.MAX_VALUE;
+            int rightElement2 = mid2<n2?nums2[mid2] : Integer.MAX_VALUE;
+            if(leftElement1<=rightElement2 && leftElement2<=rightElement1){
+                if((n1+n2)%2==1) return (double) Math.max(leftElement1,leftElement2);
+                else{
+                         return (double)(Math.max(leftElement1,leftElement2)
+                       + Math.min(rightElement1,rightElement2))/2.0;
+                }
+            }
+            if(leftElement1>rightElement2) high=mid1-1;
+            //leftElement2>rightElement1
+            else low=mid1+1;
         }
-        return 0; //dummy statement
+        return 0;
     }
 }
