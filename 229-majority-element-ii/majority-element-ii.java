dd6849d9da271657e7f7160dda95fd463959945f
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-21 21:37:20 +0530

    Time: 2 ms (99.53%) | Memory: 47.9 MB (50.30%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer>result=new ArrayList<>();
         int cnt1=0,cnt2=0;
-        int ele1=nums[0],ele2=nums[0],n=nums.length;
-        for(int i=0;i<n;i++){
-            if(cnt1==0 && nums[i]!=ele2){
-                ele1=nums[i];
+        int ele1=Integer.MIN_VALUE,ele2=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==ele1){
                 cnt1++;
+            }
+            else if(nums[i]==ele2){
+                cnt2++;
+            }
+            else if(cnt1==0 && nums[i]!=ele2){
+                cnt1=1;
+                ele1=nums[i];
             }
             else if(cnt2==0 && nums[i]!=ele1){
+                cnt2=1;
                 ele2=nums[i];
-                cnt2++;
             }
-            else if(ele1==nums[i]){
-                cnt1++;
-            }
-            else if(ele2==nums[i]){
-                cnt2++;
-            }else{
+            else{
                 cnt1--;
                 cnt2--;
             }
         }
-        int cnt12=0,cnt22=0;
-        int req=(n/3)+(n%3);
-        for(int i=0;i<n;i++){
-            if(ele1==nums[i]) cnt12++;
-            else if(ele2==nums[i]) cnt22++;
+        int cntInd1=0,cntInd2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==ele1) cntInd1++;
+            if(nums[i]==ele2) cntInd2++;
         }
-        if(cnt12> n/3){
-                list.add(ele1);
-            }
-            if(cnt22>n/3){
-                list.add(ele2);
-            }
-        return list;
+        int n=nums.length;
+        if(cntInd1>n/3){
+            result.add(ele1);
+        }
+        if(cntInd2>n/3){
+            result.add(ele2);
+        }
+        return result;
     }
 }
