5f04215114c2061cf71255fcd73dbd564dc45d57
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-22 22:38:46 +0530

    Time: 1 ms (99.78%) | Memory: 53.3 MB (21.75%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int majorityElement(int[] v) {
-       //size of the given array:
-        int n = v.length;
-        int cnt = 0; // count
-        int el = 0; // Element
-        //applying the algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = v[i];
-            } else if (el == v[i]) cnt++;
-            else cnt--;
+    public int majorityElement(int[] nums) {
+        int cnt=0,ele=nums[0],n=nums.length;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                ele=nums[i];
+                cnt++;
+            }
+            else if(nums[i]==ele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
-
-        //checking if the stored element
-        // is the majority element:
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el) cnt1++;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele) count++;
         }
-
-        if (cnt1 > (n / 2)) return el;
-        return -1; 
+        if(count>n/2) return ele;
+        return -1;
     }
 }
