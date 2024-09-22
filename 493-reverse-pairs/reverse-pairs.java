60cbe1640be76906ce027a697b7213728a3c296b
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-22 12:13:36 +0530

    Time: 79 ms (34.34%) | Memory: 53.9 MB (89.20%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,48 +1,65 @@
 class Solution {
-    public void merge(int[] nums,int low,int mid,int high){
-        int left=low,right=mid+1;
-        ArrayList<Integer>list=new ArrayList<>();
-        while(left<=mid && right<=high){
-            if(nums[left]<nums[right]){
-                list.add(nums[left]);
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
                 left++;
-            }else{
-                list.add(nums[right]);
+            } else {
+                temp.add(arr[right]);
                 right++;
             }
         }
-        while(left<=mid){
-            list.add(nums[left]);
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
             left++;
         }
-        while(right<=high){
-            list.add(nums[right]);
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
             right++;
         }
-        for(int i=low;i<=high;i++){
-            nums[i]=list.get(i-low);
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
         }
     }
-    public int countPairs(int[] nums,int low,int mid,int high){
-        int cnt=0;
-        int right=mid+1;
-        for(int i=low;i<=mid;i++){
-            while(right<=high && nums[i]> 2L*nums[right]) right++;
-            cnt+=right-(mid+1);//important
+
+    public static int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) arr[i] > 2L * arr[right])
+ right++;
+            cnt += (right - (mid + 1));
         }
         return cnt;
     }
-    public int mergeSort(int[] nums,int low,int high){
-        int cnt=0;
-        if(low>=high) return cnt;
-        int mid=(low+(high-low)/2);
-        cnt+=mergeSort(nums,low,mid);
-        cnt+=mergeSort(nums,mid+1,high);
-        cnt+=countPairs(nums,low,mid,high);
-        merge(nums,low,mid,high);
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += countPairs(arr, low, mid, high); //Modification
+        merge(arr, low, mid, high);  // merging sorted halves
         return cnt;
     }
-    public int reversePairs(int[] nums) {
-        return mergeSort(nums,0,nums.length-1);
+    
+    public int reversePairs(int[] skill) {
+        int n = skill.length;
+        return mergeSort(skill, 0, n - 1);
     }
 }
