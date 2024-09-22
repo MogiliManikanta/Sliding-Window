9c9b3329e060b7632694cad5eadb779aebc8d1a9
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-22 11:54:31 +0530

    Time: 83 ms (13.25%) | Memory: 54.1 MB (79.06%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,16 +1,9 @@
- 
-    
-
-
 class Solution {
-
     public static void merge(int[] arr, int low, int mid, int high) {
         ArrayList<Integer> temp = new ArrayList<>(); // temporary array
         int left = low;      // starting index of left half of arr
         int right = mid + 1;   // starting index of right half of arr
-
         //storing elements in the temporary array in a sorted manner//
-
         while (left <= mid && right <= high) {
             if (arr[left] <= arr[right]) {
                 temp.add(arr[left]);
@@ -20,37 +13,31 @@ class Solution {
                 right++;
             }
         }
-
         // if elements on the left half are still left //
-
         while (left <= mid) {
             temp.add(arr[left]);
             left++;
         }
-
         //  if elements on the right half are still left //
         while (right <= high) {
             temp.add(arr[right]);
             right++;
         }
-
         // transfering all elements from temporary to arr //
         for (int i = low; i <= high; i++) {
             arr[i] = temp.get(i - low);
         }
     }
-
     public static int countPairs(int[] arr, int low, int mid, int high) {
         int right = mid + 1;
         int cnt = 0;
         for (int i = low; i <= mid; i++) {
             while (right <= high && (long) arr[i] > 2L * arr[right])
- right++;
+                right++;
             cnt += (right - (mid + 1));
         }
         return cnt;
     }
-
     public static int mergeSort(int[] arr, int low, int high) {
         int cnt = 0;
         if (low >= high) return cnt;
@@ -61,7 +48,6 @@ class Solution {
         merge(arr, low, mid, high);  // merging sorted halves
         return cnt;
     }
-    
     public int reversePairs(int[] skill) {
         int n = skill.length;
         return mergeSort(skill, 0, n - 1);
