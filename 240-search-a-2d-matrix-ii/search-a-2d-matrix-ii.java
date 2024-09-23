7db6a4126a33ce201edc4cc55047f76771f68f9f
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-23 22:15:36 +0530

    Time: 5 ms (99.88%) | Memory: 45.6 MB (82.24%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,26 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix==null || matrix.length<1 || matrix[0].length<1){
-            return false;
-        }
+        int n=matrix.length;
+        int m=matrix[0].length;
         int row=0;
-        int col=matrix[0].length-1;
-        while(col>=0 && row<matrix.length){
-            if(matrix[row][col]==target){
+        int column=matrix[0].length-1;
+        while(row<n && column>=0){
+            if(matrix[row][column]==target){
                 return true;
             }
-            else if(matrix[row][col]>target){
-                col--;
+            else if(matrix[row][column]<target){
+                row++;
             }
             else{
-                row++;
+                column--;
             }
         }
         return false;
     }
 }
-/** 
-
-We start search the matrix from top right corner, initialize the current position to top right corner, if the target is greater than the value in current position, then the target can not be in entire row of current position because the row is sorted, if the target is less than the value in current position, then the target can not in the entire column because the column is sorted too. We can rule out one row or one column each time, so the time complexity is O(m+n)
-
- */
