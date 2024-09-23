b2fcbeffc38b4b773b8cc693b4590f19a6fcf4b4
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-23 22:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 88.9 MB (66.06%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,26 +1,74 @@
-class Solution {
+/*class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int startCol = 0, endCol = mat[0].length-1;
         
-        while(startCol <= endCol){
-            int maxRow = 0, midCol = startCol + (endCol-startCol)/2;
-            
-            for(int row=0; row<mat.length; row++){
-                 maxRow = mat[row][midCol] >= mat[maxRow][midCol]? row : maxRow;  
-            }
-            
-            boolean leftIsBig    =   midCol-1 >= startCol  &&  mat[maxRow][midCol-1] > mat[maxRow][midCol];
-            boolean rightIsBig   =   midCol+1 <= endCol    &&  mat[maxRow][midCol+1] > mat[maxRow][midCol];
-            
-            if(!leftIsBig && !rightIsBig)   // we have found the peak element
-                return new int[]{maxRow, midCol};
-            
-            else if(rightIsBig)  // if rightIsBig, then there is an element in 'right' that is bigger than all the elements in the 'midCol', 
-                startCol = midCol+1; //so 'midCol' cannot have a 'peakPlane'
-            
-            else // leftIsBig
-                endCol = midCol-1;
-        }
-        return null;
     }
+}*/
+public class Solution {
+
+    public int findMaxelement(int[][]mat , int n , int m , int col){
+
+        int maxValue=-1;
+
+        int index=-1;
+
+ 
+
+        for(int i=0 ; i<n ; i++){
+
+            if(mat[i][col]>maxValue){
+
+                maxValue=mat[i][col];
+
+                index=i;
+
+            }
+
+        }
+
+        return index;
+
+    }
+
+    public  int[] findPeakGrid(int [][]G){
+
+        // Write your code here.
+
+        int n=G.length;
+
+        int m=G[0].length;
+
+ 
+
+        int low=0 , high=m-1;
+
+        while(low<=high){
+
+            int mid=(low+high)/2;
+
+            int rowMax=findMaxelement(G, n, m, mid);
+
+            int left=0<=mid-1?G[rowMax][mid-1]:-1;
+
+            int right=mid+1<m?G[rowMax][mid+1]:-1;
+
+ 
+
+            if(G[rowMax][mid]>left && G[rowMax][mid]>right){
+
+                return new int[] {rowMax , mid};
+
+            }
+
+            else if(G[rowMax][mid]<left)   high=mid-1;
+
+            else low=mid+1;
+
+        }
+
+        return new int[]{-1 , -1};
+
+ 
+
+    }
+
 }
