693b3b7aacf7d0c3161d3b6166a34ea7fe1ecd6e
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-25 23:59:02 +0530

    Time: 7 ms (82.41%) | Memory: 42.6 MB (52.68%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,15 +5,13 @@ class Solution {
         while(right<s.length()){
             letters[s.charAt(right)-'A']++;
             maxFreq = Math.max(maxFreq,letters[s.charAt(right)-'A']);
-            if(right-left+1-(maxFreq)>k){
-                while(right-left+1-(maxFreq)>k){
+            while(right-left+1-(maxFreq)>k){
                     letters[s.charAt(left)-'A']--;
                     // for(int i=0;i<26;i++){
                     //     maxFreq=Math.max(maxFreq,letters[i]);
                     // }
                     left++;
                 }
-            }
             if(right-left+1-(maxFreq)<=k){
                 maxi=Math.max(right-left+1,maxi);
             }
