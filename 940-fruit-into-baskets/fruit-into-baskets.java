bcf320c6608653156592300f675209d6100ed575
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-25 22:30:39 +0530

    Time: 51 ms (43.51%) | Memory: 55.2 MB (35.34%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,11 +1,32 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        Map<Integer,Integer> map =new HashMap<>();
+        int left=0,right=0,maxi=0;
+        while(right<fruits.length){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            if(map.size()>2){
+                map.put(fruits[left],map.get(fruits[left])-1);
+                if(map.get(fruits[left])==0){
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+            if(map.size()<=2){
+                maxi=Math.max(maxi,right-left+1);
+            }
+            right++;
+        }
+        return maxi;
+    }
+}
+/*class Solution {
+    public int totalFruit(int[] fruits) {
         Map<Integer,Integer>map=new HashMap<>();
         int left=0,right=0,maxi=0;
         while(right<fruits.length){
              map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
             if(map.size()>2){
-                while(map.size()>2){
+                while(left<right && map.size()>2){
                     map.put(fruits[left],map.get(fruits[left])-1);
                     if(map.get(fruits[left])==0){
                         map.remove(fruits[left]);
@@ -20,7 +41,7 @@ class Solution {
         }
         return maxi;
     }
-}
+}*/
 /**class Solution {
     public int totalFruit(int[] fruits) {
         int maxi=0;
