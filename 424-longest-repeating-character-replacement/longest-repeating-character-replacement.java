6b4ae34509888564044913716951a9eb81cdadf7
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-25 23:58:29 +0530

    Time: 7 ms (82.41%) | Memory: 42.6 MB (44.30%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -8,6 +8,29 @@ class Solution {
             if(right-left+1-(maxFreq)>k){
                 while(right-left+1-(maxFreq)>k){
                     letters[s.charAt(left)-'A']--;
+                    // for(int i=0;i<26;i++){
+                    //     maxFreq=Math.max(maxFreq,letters[i]);
+                    // }
+                    left++;
+                }
+            }
+            if(right-left+1-(maxFreq)<=k){
+                maxi=Math.max(right-left+1,maxi);
+            }
+            right++;
+        }
+        return maxi;
+    }
+}/*class Solution {
+    public int characterReplacement(String s, int k) {
+        int letters[] = new int[26];
+        int left=0,right=0,maxi=0,maxFreq=0;
+        while(right<s.length()){
+            letters[s.charAt(right)-'A']++;
+            maxFreq = Math.max(maxFreq,letters[s.charAt(right)-'A']);
+            if(right-left+1-(maxFreq)>k){
+                while(right-left+1-(maxFreq)>k){
+                    letters[s.charAt(left)-'A']--;
                     for(int i=0;i<26;i++){
                         maxFreq=Math.max(maxFreq,letters[i]);
                     }
@@ -21,7 +44,7 @@ class Solution {
         }
         return maxi;
     }
-}
+}*/
 /*class Solution {
     public int characterReplacement(String s, int k) {
         int maxi=0,maxFreq=0;
