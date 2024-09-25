ea353802ed12e2af05b1fe2585cbcd3512994873
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-25 23:57:20 +0530

    Time: 6 ms (92.70%) | Memory: 42.6 MB (52.68%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,5 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        int letters[] = new int[26];
+        int left=0,right=0,maxi=0,maxFreq=0;
+        while(right<s.length()){
+            letters[s.charAt(right)-'A']++;
+            maxFreq = Math.max(maxFreq,letters[s.charAt(right)-'A']);
+            if(right-left+1-(maxFreq)>k){
+                while(right-left+1-(maxFreq)>k){
+                    letters[s.charAt(left)-'A']--;
+                    for(int i=0;i<26;i++){
+                        maxFreq=Math.max(maxFreq,letters[i]);
+                    }
+                    left++;
+                }
+            }
+            if(right-left+1-(maxFreq)<=k){
+                maxi=Math.max(right-left+1,maxi);
+            }
+            right++;
+        }
+        return maxi;
+    }
+}
+/*class Solution {
+    public int characterReplacement(String s, int k) {
         int maxi=0,maxFreq=0;
         for(int i=0;i<s.length();i++){
             int[] letters = new int[26];
@@ -16,4 +40,4 @@ class Solution {
         }
         return maxi;
     }
-}
+}*/
