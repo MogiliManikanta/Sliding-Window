f9f5790e9a81d5d375f5de87d45ea10dcbe038fa
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-26 20:58:01 +0530

    Time: 3 ms (100.00%) | Memory: 47.7 MB (25.91%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,25 +1,42 @@
 class Solution {
+    // public int subarraysWithKDistinct(int[] nums, int k) {
+    //     return atmost(nums, k) - atmost(nums, k-1);
+    // }
+    
+    // private int atmost(int[] nums, int k) {
+    //     Map<Integer, Integer> m = new HashMap<>();
+    //     int start = 0;
+    //     int ans = 0;
+    //     for(int end=0; end<nums.length; end++) {
+    //         m.put(nums[end], m.getOrDefault(nums[end], 0) + 1);
+    //         while (m.size() > k) {
+    //             int tmp = m.get(nums[start]);
+    //             if (tmp==1) m.remove(nums[start++]);
+    //             else m.put(nums[start++], tmp-1);
+    //         }
+    //         ans += (end-start+1);
+    //     }
+    //     return ans;
+    // }
+
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMostKInteger(nums,k)-atMostKInteger(nums,k-1);
-    }
-    public int atMostKInteger(int[] nums,int k){
-        HashMap<Integer,Integer>count = new HashMap<>();
-        int i=0,result=0;
-        int n=nums.length;
-        for(int j=0;j<n;j++){
-            if(count.getOrDefault(nums[j],0)==0){
-                k--;
+        int[] frq = new int[nums.length+1];
+        int start = 0, curr = 0, ans = 0;
+        for(int end=0; end<nums.length; end++) {
+            if (frq[nums[end]]++ == 0) k--;
+            while (k<0) {
+               --frq[nums[start++]];
+               k++;
+               curr = 0;
             }
-            count.put(nums[j],count.getOrDefault(nums[j],0)+1);
-            while(k<0){
-                count.put(nums[i],count.get(nums[i])-1);
-                if(count.getOrDefault(nums[i],0)==0){
-                    k++;
+            if (k==0) {
+                while(frq[nums[start]] > 1) {
+                    --frq[nums[start++]];
+                    curr++;
                 }
-                i++;
+                ans += curr + 1;
             }
-            result+=j-i+1;
         }
-        return result;
+        return ans;
     }
 }
