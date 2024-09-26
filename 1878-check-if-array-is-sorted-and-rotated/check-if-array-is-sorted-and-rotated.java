c537d8ed600cb40961c471e847c5b4c5eb804406
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-26 22:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (38.67%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,13 +1,24 @@
 class Solution {
     public boolean check(int[] nums) {
-        int drop=0,n=nums.length;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]>nums[i+1]) {
-                drop++;
-            }
+        int n = nums.length, dropPoint = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) dropPoint++;
         }
-        if(drop==0) return true;
-        else if(drop==1 && nums[0]>=nums[n-1]) return true;
-        else return false;
+        if (dropPoint == 0) return true;
+        if (dropPoint == 1 && nums[0] >= nums[n - 1]) return true;
+        return false;
     }
 }
+
+/**
+NonSequence = nums[i] > nums[i+1] i.e. number is greater than its right.
+Find non sequences count. 
+-- In case of sorted array there will be 0 nonsequences. 
+   [1,2,3,4,5]
+-- In case of rotated sorted array there will be 1 nonsequence.
+   [3,4,5,1,2]
+-- In other cases like [2,1,3,4] nonsequences > 1 ==> 2
+
+   How to count 4 > 2, in that case we need nums[i] > nums[(i+1)%len]
+
+*****/
