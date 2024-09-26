6047b1d307196d9afc737e7b6c8ba5807bebb87c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-26 19:38:58 +0530

    Time: 13 ms (50.22%) | Memory: 44.8 MB (25.01%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -4,9 +4,9 @@ class Solution {
         int cnt=0;
         for(int i=0;i<s.length();i++){
             hash[s.charAt(i)-'a']=i;
-            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1) {
+            // if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1) {
                 cnt+=(1 + Math.min(hash[0],Math.min(hash[1],hash[2])));
-            }
+            // }
         }
         return cnt;
     }
