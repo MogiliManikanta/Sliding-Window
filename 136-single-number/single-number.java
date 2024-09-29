4e6a6373dee9ce8f953f97264f639f30c067037a
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-29 22:19:00 +0530

    Time: 1 ms (99.79%) | Memory: 45.6 MB (80.01%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,19 +1,9 @@
 class Solution {
-    public int singleNumber(int[] A) {
-         int result = 0;
-    for (int i = 0; i<A.length; i++)
-    {
-		result ^=A[i];
-    }
-	return result;
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++) {
+            xor^=nums[i];
+        }
+        return xor;
     }
 }
-/**Similarly , if we observe ,
-
-A^A=0
-A^B^A=B
-(A^A^B) = (B^A^A) = (A^B^A) = B This shows that position doesn't matter.
-Similarly , if we see , a^a^a^a......... (even times)=0 and a^a^a........(odd times)=a
-
-
- */
