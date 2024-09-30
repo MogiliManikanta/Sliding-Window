14d7795c745280b5afb7dcbac47799b1b831703c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-30 19:39:59 +0530

    Time: 1 ms (34.48%) | Memory: 40.1 MB (73.45%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.java
+++ 556-next-greater-element-iii/next-greater-element-iii.java
@@ -1,44 +1,47 @@
 class Solution {
-    public int nextGreaterElement(int n) {
-        String num = n+"";
-        int index1=-1;
-        int index2=-1;
-        int size=num.length();
-        char[] nums = num.toCharArray();
-        for(int i=size-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
+    public int nextGreaterElement(int n1) {
+        String s = n1+"";
+        char nums[] = s.toCharArray();
+        int index1=-1,index2=-1,n=nums.length;
+        for(int i=n-2;i>=0;i--) {
+            if(nums[i] < nums[i+1]) {
                 index1=i;
                 break;
             }
         }
-        if(index1==-1){
+        if(index1==-1) {
             return -1;
+            // reverse(nums,0);
         }
-        // int index2=-1;
-        for(int i=size-1;i>=index1;i--){
-            if(nums[i]>nums[index1]){
-                swap(nums,index1,i);
-                index2=i;
-                break;
+        else {
+            for(int i=n-1;i>=index1;i--) {
+                if(nums[i]>nums[index1]) {
+                    index2=i;
+                    break;
+                }
             }
+            swap(nums,index1,index2);
+            reverse(nums,index1+1);
         }
-        reverse(nums,index1+1,size-1);
-        String s = new String(nums);
-        long k=Long.parseLong(s);
-        return k>Integer.MAX_VALUE?-1:(int)k;
+        // int number = Integer.parseInt(nums.toString());
+        long number = Long.parseLong(new String(nums));
+
+        return number > Integer.MAX_VALUE?-1:(int)number;
+        
     }
-    public void reverse(char[] nums,int left,int right){
-        while(left<right){
-            char temp=nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
+    public void swap(char[] nums,int index1,int index2) {
+        char temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+    public void reverse(char[] nums,int left) {
+        int right=nums.length-1;
+        while(left<right) {
+            char temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
             left++;
             right--;
         }
-    }
-    public void swap(char[] nums,int left,int right){
-        char temp=nums[left];
-        nums[left]=nums[right];
-        nums[right]=temp;
     }
 }
