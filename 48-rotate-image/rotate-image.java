0efe351aa9d0753aae93768c6a8d4a5b3e650569
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-09-30 22:20:26 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (61.71%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,27 +1,25 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int row=matrix.length;
-        int column=matrix[0].length;
-        // step 1 : Transpose the matrix
+        int row=matrix.length,column=matrix[0].length;
         for(int i=0;i<row;i++){
-            for(int j=i+1;j<column;j++){
-                // swap the matrix[i][j] to matrix[j][i]
+            for(int j=i+1;j<column;j++) {
                 int temp = matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
-
-        // 1 2 3        1 4 7        7 4 1
-        // 4 5 6    ==> 2 5 8   ==>  8 5 2
-        // 7 8 9        3 6 9        9 6 3
-        // swap the columns in place of matrix using in place algorithm
         for(int i=0;i<row;i++){
-            for(int j=0;j<column/2;j++){
-                int temp = matrix[i][j];
+            for(int j=0;j<column/2;j++) {
+                int temp=matrix[i][j];
                 matrix[i][j] = matrix[i][column-1-j];
                 matrix[i][column-1-j]=temp;
             }
         }
     }
 }
+
+//
+
+// 1 4 7
+// 2 5 8
+// 3 6 9
