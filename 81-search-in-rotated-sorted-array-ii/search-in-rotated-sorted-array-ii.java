bda17b206229ec1eb4b945b9e14cab7629d827ec
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-02 23:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (70.24%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,28 +1,34 @@
 class Solution {
-    public boolean search(int[] arr, int target) {
-       int low = 0, high = arr.length - 1; //<---step 1
-
-        while (low <= high) { //<--- step 2
-            int mid = (low + high) / 2; //<----step 3
-            if (arr[mid] == target)
-                return true; // <---step 4
-            if(arr[low]==arr[mid] && arr[mid]==arr[high]){ //only difference is here only
+    public boolean search(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        while(low <= high) {
+            int mid = (low+(high-low)/2);
+            if(nums[mid]==target) {
+                return true;
+            }
+            else if(nums[low]==nums[mid] && nums[mid]==nums[high]) {
                 low++;
                 high--;
                 continue;
             }
-            if (arr[low] <= arr[mid]) { //<---step 5
-                if (arr[low] <= target && arr[mid] >= target)
-                    high = mid - 1; //<---step 6 
-                else
-                    low = mid + 1; //<---step 7
-            } else { //<---step 7
-                if (arr[mid] <= target && target <= arr[high])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
+            
+            else if(nums[low]<=nums[mid]) {
+                if(nums[low]<=target && target <= nums[mid]) {
+                    high = mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(target >= nums[mid] && nums[high] >= target) {
+                    low=mid+1;
+                }
+                else {
+                    high=mid-1;
+                }
             }
         }
-        return false; //<---step 8 
+        return false;
     }
 }
