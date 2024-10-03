331ba9f02dbf8ff70e390f8661eb40d96074ae02
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-03 22:33:18 +0530

    Time: 7 ms (91.57%) | Memory: 48.5 MB (12.12%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,33 +1,27 @@
 class Solution {
-    public int smallestDivisor(int[] arr, int limit) {
-        int n = arr.length; //size of array.
-        if(n > limit) return -1;
-        //Find the maximum element:
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, arr[i]);
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low=1,high=-(int)1e9;
+        for(int i=0;i<nums.length;i++) {
+            high=Math.max(high,nums[i]);
         }
-
-        int low = 1, high = maxi;
-
-        //Apply binary search:
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (sumByD(arr, mid) <= limit) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int ans=0;
+        while(low<=high) {
+            int mid=(low + (high-low)/2);
+            if(possible(mid,nums,threshold)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        return low;
+        return ans;
     }
-    public int sumByD(int[] arr, int div) {
-        int n = arr.length; //size of array
-        //Find the summation of division values:
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil((double)(arr[i]) / (double)(div));
+    public boolean possible(int mid,int[] nums,long threshold) {
+        long divisors=0;
+        for(int i=0;i<nums.length;i++) {
+            divisors += (nums[i]+mid-1)/mid;
         }
-        return sum;
+        return divisors<=threshold;
     }
 }
