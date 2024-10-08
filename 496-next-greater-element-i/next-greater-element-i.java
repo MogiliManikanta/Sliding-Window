0eebb5483d7218e3577f079e7302505fcd5c0864
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-08 18:55:54 +0530

    Time: 3 ms (92.80%) | Memory: 43.3 MB (75.56%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int[] nextGreaterElement(int[] findNums, int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
-        Stack<Integer> stack = new Stack<>();
-        for (int num : nums) {
-            while (!stack.isEmpty() && stack.peek() < num)
-                map.put(stack.pop(), num);
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        Stack<Integer>stack=new Stack<>();
+        for(int num : nums2) {
+            while(!stack.isEmpty() && stack.peek()<num){
+                map.put(stack.pop(),num);
+            }
             stack.push(num);
-        }   
-        for (int i = 0; i < findNums.length; i++)
-            findNums[i] = map.getOrDefault(findNums[i], -1);
-        return findNums;
+        }
+        int[] result = new int[nums1.length];
+        for(int i=0;i < nums1.length;i++) {
+            result[i] = map.getOrDefault(nums1[i],-1);
+        }
+        return result;
     }
 }
