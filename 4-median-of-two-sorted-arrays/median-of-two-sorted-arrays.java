d35ce01aba1bf075e89bd7e8ce814135bc674b75
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-08 21:25:04 +0530

    Time: 2 ms (62.84%) | Memory: 46.1 MB (25.86%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int n=nums1.length;
+        int m=nums2.length;
+        if(n>m) return findMedianSortedArrays(nums2,nums1);
         int low=0;
-        int high=n1;
-        int left=(n1+n2+1)/2;
-        while(low<=high){
-            int mid1=(low+high)/2;
-            int mid2=(left-mid1);
-            int leftElement1 = mid1-1>=0?nums1[mid1-1] : Integer.MIN_VALUE;
-            int leftElement2 = mid2-1>=0?nums2[mid2-1] : Integer.MIN_VALUE;
-            int rightElement1 = mid1<n1?nums1[mid1] :Integer.MAX_VALUE;
-            int rightElement2 = mid2<n2?nums2[mid2] : Integer.MAX_VALUE;
-            if(leftElement1<=rightElement2 && leftElement2<=rightElement1){
-                if((n1+n2)%2==1) return (double) Math.max(leftElement1,leftElement2);
+        int high=n;
+        int left=(n+m+1)/2;
+        while(low <= high) {
+            int mid1 = (low + (high-low)/2);
+            int mid2 = left-mid1;
+            int leftElement1 = mid1>0 ? nums1[mid1-1] :Integer.MIN_VALUE;
+            int rightElement1 = mid1<n?nums1[mid1] : Integer.MAX_VALUE;
+            int leftElement2 = mid2>0 ? nums2[mid2-1] : Integer.MIN_VALUE;
+            int rightElement2=mid2<m?nums2[mid2] : Integer.MAX_VALUE;
+            if(leftElement1<=rightElement2 && leftElement2 <= rightElement1){
+                if((m+n)%2==0) {
+                     return (Math.max(leftElement1,leftElement2) + Math.min(rightElement1,rightElement2))/2.0;
+                }
                 else{
-                         return (double)(Math.max(leftElement1,leftElement2)
-                       + Math.min(rightElement1,rightElement2))/2.0;
+                    return Math.max(leftElement1,leftElement2);
                 }
             }
-            if(leftElement1>rightElement2) high=mid1-1;
-            //leftElement2>rightElement1
-            else low=mid1+1;
+            else if(leftElement1 < rightElement2) {
+                low=mid1+1;
+            }
+            else{
+                high=mid1-1;
+            }
         }
-        return 0;
+        return 0.0;
     }
 }
