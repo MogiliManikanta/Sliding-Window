1b51929196977eba2d1f17015ea801af30d967a6
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-12 18:44:50 +0530

    Time: 34 ms (37.73%) | Memory: 52.7 MB (16.24%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        int n=nums.length;
-        for(int i=0;i<n;i++){
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+        for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum>0){
-                    k--;
+            int left=i+1,right=n-1;
+            while(left<right){
+                long sum=nums[i];
+                sum+=nums[left];
+                sum+=nums[right];
+                if(sum==0){
+                    ArrayList<Integer>temp=new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    list.add(temp);
+                    left++;right--;
+                    while(left<right && nums[left]==nums[left-1]) left++;
+                    while(left<right && nums[right]==nums[right+1]) right--;
                 }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    // List<Integer> list = new ArrayList<>();
-                    result.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    // result.add(list);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--; 
-                }
+                else if(sum<0) left++;
+                else right--;
             }
         }
-        return result;
+        return list;
     }
 }
