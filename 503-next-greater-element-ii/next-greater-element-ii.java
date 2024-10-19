c91550e2ac8fdd64d98b0fe1684a45144fc438da
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-19 23:21:19 +0530

    Time: 17 ms (49.57%) | Memory: 46.2 MB (48.18%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,4 +1,25 @@
 class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Stack<Integer>stack = new Stack<>();
+        for(int i=2*n-1;i>=0;i--) {
+            while(!stack.isEmpty() && stack.peek()<=nums[i%n]) {
+                stack.pop();
+            }
+            if(i<n){
+            if(stack.isEmpty()) {
+                result[i]=-1;
+            }
+            else{
+                result[i] = stack.peek();
+            }}
+            stack.push(nums[i%n]);
+        }
+        return result;
+    }
+}
+/*class Solution {
     public int[] nextGreaterElements(int[] arr) {
         int n=arr.length;
         int[] result = new int[n];
@@ -13,4 +34,4 @@ class Solution {
         }
         return result;
     }
-}
+}*/
