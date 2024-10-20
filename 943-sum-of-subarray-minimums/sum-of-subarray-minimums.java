8eb7abdd371067689059a60efb2d7be68f2e57c2
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-20 23:17:16 +0530

    Time: 76 ms (15.53%) | Memory: 51.5 MB (37.87%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,57 +1,68 @@
-import java.util.*;
-
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int length = arr.length;
-        int[] prevSmaller = calculatePrevSmallerElements(arr);
-        int[] nextSmaller = calculateNextSmallerElements(arr);
-      
-        int mod = (int) 1e9 + 7;
-        long answer = 0;
-      
-        for (int i = 0; i < length; ++i) {
-            answer += (long) (i - prevSmaller[i]) * (nextSmaller[i] - i) % mod * arr[i] % mod;
+        int n=arr.length;
+        int []nse = nextSmallerElement(arr,n);
+        int []pse = previousSmallerElement(arr,n);
+        long answer=0;
+        int mod = (int)1e9+7;
+        for(int i=0;i<n;i++) {
+            long leftLen = i-pse[i];
+            long rightLen = nse[i]-i;
+            // result +=  (leftLen * rightLen * arr[i])%mod;
+            answer += (long) (i - pse[i]) * (nse[i] - i) % mod * arr[i] % mod;
             answer %= mod;
         }
-      
-        return (int) answer;
+        return (int)(answer%mod);
     }
-  
-    private int[] calculatePrevSmallerElements(int[] arr) {
-        int length = arr.length;
-        int[] prevSmaller = new int[length];
-        Arrays.fill(prevSmaller, -1);
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        for (int i = 0; i < length; ++i) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+    public int[] previousSmallerElement(int[] arr,int n) {
+        int pse[] = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        Arrays.fill(pse,-1);
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
                 stack.pop();
             }
-            if (!stack.isEmpty()) {
-                prevSmaller[i] = stack.peek();
+            if(stack.isEmpty()) {
+                pse[i] = -1;
+            }
+            else {
+                pse[i]=stack.peek();
             }
             stack.push(i);
         }
-      
-        return prevSmaller;
+        return pse;
     }
-  
-    private int[] calculateNextSmallerElements(int[] arr) {
-        int length = arr.length;
-        int[] nextSmaller = new int[length];
-        Arrays.fill(nextSmaller, length);
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+
+    public int[] nextSmallerElement(int[] arr,int n) {
+        int[] nse = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        Arrays.fill(nse,n);
+        for(int i=n-1;i>=0;i--) {
+            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]) {
                 stack.pop();
             }
-            if (!stack.isEmpty()) {
-                nextSmaller[i] = stack.peek();
+            if(stack.isEmpty()) {
+                nse[i]=n;
+            }
+            else{
+                nse[i]=stack.peek();
             }
             stack.push(i);
         }
-      
-        return nextSmaller;
+        return nse;
     }
 }
+/**class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int result = 0;
+        int mod = (int) 1e9+7;
+        for(int i=0;i<arr.length;i++) {
+            int mini = arr[i];
+            for(int j=i;j<arr.length;j++) {
+                mini = Math.min(arr[j],mini);
+                result= (result + mini)%mod;
+            }
+        }
+        return result;
+    }
+} */
