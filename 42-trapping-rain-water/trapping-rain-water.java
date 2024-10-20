48aad1758fd240c36cb11bbb3d4c0ca4ca66762e
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-20 22:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (38.21%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,4 +1,34 @@
 class Solution {
+    public int trap(int[] height) {
+       int left=0,right=height.length-1;
+       int leftMax = 0,rightMax = 0;
+       int result = 0;
+       while(left<right){
+        if(height[left]<=height[right]) {
+            if(height[left]>leftMax) {
+                leftMax = height[left];
+            }
+            else{
+                result+=leftMax-height[left];
+            }
+            left++;
+        }
+        else{
+           if(height[right]>rightMax) {
+            rightMax = height[right];
+           } 
+           else {
+            result += rightMax - height[right];
+           }
+           right--;
+        }
+       }
+       return result; 
+    }
+}
+/**
+
+class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
         int[] suffix = new int[n];
@@ -20,4 +50,4 @@ class Solution {
         }
         return waterTrapped;
     }
-}
+} */
