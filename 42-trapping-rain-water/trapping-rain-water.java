19f311b013358a3a5d0a2e71419ee6ac618b78cd
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-20 22:18:40 +0530

    Time: 1 ms (67.38%) | Memory: 46.4 MB (38.21%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -2,19 +2,21 @@ class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
         int[] suffix = new int[n];
-        int[] prefix = new int[n];
-        prefix[0]=arr[0];
-        for(int i=1;i<n;i++) {
-            prefix[i] = Math.max(prefix[i-1],arr[i]); 
-        }
+        // int[] prefix = new int[n];
+        // prefix[0]=arr[0];
+        // for(int i=1;i<n;i++) {
+        //     prefix[i] = Math.max(prefix[i-1],arr[i]); 
+        // }
         suffix[n-1]=arr[n-1];
         for(int i=n-2;i>=0;i--) {
             suffix[i]=Math.max(suffix[i+1],arr[i]);
         }
         int left=0,right=n-1;
         int waterTrapped=0;
+        int leftSum = arr[0];
         for(int i=0;i<n;i++) {
-            waterTrapped += Math.min(prefix[i],suffix[i]) - arr[i];
+            leftSum=Math.max(leftSum,arr[i]);
+            waterTrapped += Math.min(leftSum,suffix[i]) - arr[i];
         }
         return waterTrapped;
     }
