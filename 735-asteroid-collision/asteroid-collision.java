54e4aa9968607665ee34ffd404e85913b24283a0
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-21 22:36:11 +0530

    Time: 5 ms (86.30%) | Memory: 45 MB (92.67%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -22,3 +22,30 @@ class Solution {
         return result;
     }
 }
+/**class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for(int ele : asteroids){
+            if(ele > 0){
+                st.push(ele);
+              // System.out.println(ele);
+            }
+            else{   
+                while(!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(ele)){   
+                    st.pop();
+                }      
+                if(!st.isEmpty() && st.peek() == Math.abs(ele)){   
+                    st.pop();
+                }
+                else if(st.isEmpty() || st.peek() < 0){   
+                    st.push(ele);
+                }
+            }
+        }   
+        int[] arr = new int[st.size()];
+        for(int i = st.size()-1 ; i>=0 ; i-- ){
+            arr[i] = st.pop();
+        }
+        return arr;
+    }
+} */
