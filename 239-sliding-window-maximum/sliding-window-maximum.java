bdcf83c99d06ce0a5d6d97270a2c41c0130244f2
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-23 22:59:36 +0530

    Time: 32 ms (52.99%) | Memory: 58.6 MB (64.25%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,38 +1,36 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {		
-        // Check for edge cases
-        if (nums == null || k <= 0) {
-            return new int[0]; // If the input is invalid, return an empty array
-        }
-        
-        int n = nums.length;
-        int[] result = new int[n - k + 1]; // Initialize the result array to store maximum values
-        int resultIndex = 0;
-        
-        // Create a deque (double-ended queue) to store the indices of elements in the sliding window
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+        int[] result = new int[n-k+1];
         Deque<Integer> deque = new ArrayDeque<>();
-        
-        for (int currentIndex = 0; currentIndex < nums.length; currentIndex++) {
-            // Remove numbers from the front of the deque that are out of the current window
-            while (!deque.isEmpty() && deque.peek() < currentIndex - k + 1) {
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(!deque.isEmpty() && deque.peek()==i-k){
                 deque.poll();
             }
-            
-            // Remove smaller numbers from the back of the deque as they are not needed for maximum calculation
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[currentIndex]) {
+            while(!deque.isEmpty() && nums[deque.peekLast()]<nums[i]){
                 deque.pollLast();
             }
-            
-            // Add the current index to the deque
-            deque.offer(currentIndex);
-            
-            // If the current index is within the first valid window (k-1 or more elements in the window),
-            // add the maximum value in the current window to the result array front
-            if (currentIndex >= k - 1) {
-                result[resultIndex++] = nums[deque.peek()];
+             deque.offer(i);
+            if(i>=k-1){
+                result[index++]=nums[deque.peek()];//deque.peekFirst() same as deque.peek();
             }
         }
-        
-        return result; // Return the array containing maximum values in each sliding window
+        return result;
     }
 }
+/**class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+        int[] result = new int[n-k+1];
+        int index=0;
+        for(int i=0;i<=n-k;i++){
+            int max=-(int)1e9;
+            for(int j=i;j<i+k;j++){
+                max=Math.max(nums[j],max);
+            }
+            result[index++]=max;
+        }
+        return result;
+    }
+} */
