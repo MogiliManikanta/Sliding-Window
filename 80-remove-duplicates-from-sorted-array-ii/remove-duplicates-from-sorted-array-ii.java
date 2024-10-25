2712208b02e02362cdf291081da1a9849744b390
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-25 19:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (49.13%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,24 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int index=0;
-        for(int n : nums){
-            if(index<2 || n>nums[index-2]){
-                nums[index++] = n;
+        for(int i=0;i<nums.length;i++){
+            if(i<2 || nums[i]>nums[index-2]){
+                nums[index++]=nums[i];
             }
         }
         return index;
     }
 }
-
-/**class Solution {
-    public int removeDuplicates(int[] nums) {
-        int index=0;
-        for(int i=0;i<nums.length;i++){
-            if(i<2 || nums[index-2]<nums[i]) {
-                nums[index]=nums[i];
-                index++;
-            }
-        }
-        return index;
-    }
-} */
