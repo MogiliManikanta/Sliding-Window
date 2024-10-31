d6f9d759e7c872c353982f88478b394fa012a473
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-31 21:20:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (46.66%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -9,33 +9,27 @@
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int N) {
-      // Create two pointers, fastp and slowp
-        ListNode fastp = head;
-        ListNode slowp = head;
-
-        // Move the fastp pointer N nodes ahead
-        for (int i = 0; i < N; i++)
-            fastp = fastp.next;
-
-        // If fastp becomes null, the Nth node from the end is the head
-        if (fastp == null)
-            return head.next;
-
-        // Move both pointers until fastp reaches the end
-        while (fastp.next != null) {
-            fastp = fastp.next;
-            slowp = slowp.next;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head==null) return head;
+        ListNode current = head;
+        int count=0;
+        while(current != null){
+            count++;
+            current=current.next;
         }
-
-        // Delete the Nth node from the end
-        ListNode delNode = slowp.next;
-        slowp.next = slowp.next.next;
-        delNode = null;
+        if(count==n){
+            return head.next;
+        }
+        current=head;
+        int res=count-n;
+        while(current != null){
+            res--;
+            if(res==0){
+                break;
+            }
+            current=current.next;
+        }
+        current.next=current.next.next;
         return head;
     }
 }
-/***A one pass solution can be done using pointers. Move one pointer fast --> n+1 places forward, to maintain a gap of n between the two pointers and then move both at the same speed. Finally, when the fast pointer reaches the end, the slow pointer will be n+1 places behind - just the right spot for it to be able to skip the next node.
-
-Since the question gives that n is valid, not too many checks have to be put in place. Otherwise, this would be necessary.
- */ 
