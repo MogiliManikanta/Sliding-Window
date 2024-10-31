2c0e28ca75e5f24e2ec4f50b47860aa10aa0e8ac
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-31 21:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (86.80%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,6 +10,25 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head==null) return null;
+        ListNode fast=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            return head.next;
+        }
+        ListNode slow = head;
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
+/**class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head==null) return head;
         ListNode current = head;
         int count=0;
@@ -32,4 +51,4 @@ class Solution {
         current.next=current.next.next;
         return head;
     }
-}
+} */
