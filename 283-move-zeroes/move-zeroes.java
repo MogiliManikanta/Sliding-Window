a07582455ce68032fcf5dd05142bc4fe86e27b81
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-11-03 20:08:39 +0530

    Time: 2 ms (84.60%) | Memory: 45.6 MB (89.40%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,15 @@
 class Solution {
-     public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
-            }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
-            }
+    public void moveZeroes(int[] nums) {
+       int swapBalls = 0;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==0){
+            swapBalls++;
         }
+        else{
+            int temp = nums[i];
+            nums[i] = 0;
+            nums[i-swapBalls] = temp;
+        }
+       }  
     }
 }
