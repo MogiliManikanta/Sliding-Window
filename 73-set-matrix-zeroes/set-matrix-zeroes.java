24fe3cc8fe66a7a16471e55593cebf875f9ec417
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-11-04 22:47:49 +0530

    Time: 1 ms (78.91%) | Memory: 45.3 MB (91.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,35 +1,37 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int row=matrix.length;
-        int column = matrix[0].length;
-        int col0=1;
-        for(int i=0;i<row;i++) {
-            for(int j=0;j<column;j++) {
-                if(matrix[i][j]==0) {
-                    matrix[i][0] =0;
+        int col0=1,row=matrix.length,col=matrix[0].length;
+        // int col[m] = matrix[...][0]
+        // int row[n] = matrix[0][...]
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    // mark the row 
+                    matrix[i][0]=0;
+                    // mark the col
                     if(j!=0){
                         matrix[0][j]=0;
-                    }
-                    else{
+                    }else{
                         col0=0;
                     }
                 }
             }
         }
-        for(int i=1;i<row;i++) {
-            for(int j=1;j<column;j++) {
-                if(matrix[i][0]==0 || matrix[0][j]==0) {
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        if(matrix[0][0]==0) {
-            for(int i=0;i<column;i++) {
-                matrix[0][i] =0;
-            } 
+        // For the column
+        if(matrix[0][0]==0){
+            for(int i=0;i<col;i++){
+                matrix[0][i]=0;
+            }
         }
-        if(col0==0) {
-            for(int i=0;i<row;i++) {
+        if(col0==0){
+            for(int i=0;i<row;i++){
                 matrix[i][0]=0;
             }
         }
@@ -37,28 +39,28 @@ class Solution {
 }
 /*class Solution {
     public void setZeroes(int[][] matrix) {
-        int row=matrix.length;
-        int column=matrix[0].length;
-        int rowAns[] = new int[row];
-        int colAns[] = new int[column];
-        for(int i=0;i<row;i++) {
-            for(int j=0;j<column;j++) {
-                if(matrix[i][j] == 0) {
-                    rowAns[i] =-1;
-                    colAns[j] =-1;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] rowAns = new int[row];
+        int[] colAns = new int[col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    rowAns[i]=-1;
+                    colAns[j]=-1;
                 }
             }
         }
-        for(int i=0;i<row;i++) {
-            for(int j=0;j<column;j++) {
-                if(rowAns[i]==-1 || colAns[j]==-1) {
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rowAns[i]==-1 || colAns[j]==-1){
                     matrix[i][j]=0;
                 }
             }
         }
     }
 }*/
-/*class Solution {
+/*****class Solution {
     public void setZeroes(int[][] matrix) {
         int col=matrix[0].length;
         int row=matrix.length;
